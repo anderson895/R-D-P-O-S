a6813e28b21563f6bc2a34dd3ba86f4e31d2eb7d
Fyke Lleva <floterina@gmail.com>
2024-07-30 03:37:49 +0800

    updated time and date

---
+++ POS/pages/dashboards_and_analytics.php
@@ -0,0 +1,28 @@
+<div class="row m-2 g-3">
+    <div class="col-12 col-md-3">
+        <div class="border p-3 dim_green" style="background-color: white; border-radius: 20px;">
+            <div class="row d-flex-wrap">
+                <div class="col-7">
+                    <h5 class="mt-3 color_red">Good Afternoon </h5>
+                    <h1 class="fw-bolder color_red" style="font-size: 3rem; margin-top: -10px; margin-bottom: -10px">12:00am</h1>
+                    <h5 class="m-0 color_red">July 30, 2024</h5>
+                </div>
+                <div class="col-5">
+                    <i style="font-size: 5rem;" class="bi color_red bi-cloud-sun"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row m-2 g-3">
+    <div class="col-12 col-md-3">
+        <div class="border p-3" style="background-color: white; border-radius: 20px;">
+            <h5 class="mb-2">Stock Levels</h5>
+            <hr>
+            <div class="d-flex text-muted m-0 flex-row justify-content-between">
+                <p>Product</p>
+                <p>Stocks</p>
+            </div>
+        </div>
+    </div>
+</div>
