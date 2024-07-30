8dc5867ad5f08fb21d3774a8e1e1e045d0c441c4
Fyke Lleva <floterina@gmail.com>
2024-07-30 13:48:20 +0800

    user statistics

---
+++ POS/pages/stock_level.php
@@ -0,0 +1,100 @@
+<div class="border color_red shadow p-3 pb-4" style="background-color: white; border-radius: 20px;">
+            <h5 class="mb-2 fw-bolder mt-3">Stock Levels</h5>
+            <div class="d-flex text-muted flex-row justify-content-between">
+                <p>Product</p>
+                <p>Stocks</p>
+            </div>
+            <hr class="mt-0">
+            <div class="row mb-3">
+                <div class="col-9">
+                    <div class="d-flex flex-row">
+                        <div class="me-2 rounded"  style="width: 30%; height: 70px">
+                            <img  class="border rounded" style="width: 100%; height: 100%; object-fit: cover" src="../../POS/pages/upload_prodImg/ambroxitil - Copy.jpg" alt="prod">
+                        </div>
+                        <div style="width: 70%;">
+                            <p class="fw-bold">Ambroxitil 150g for rooster hog pet</p>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                <div class="col-3">
+                    <p class="fw-bold text-end m-0">0 pcs</p>
+                    <div style="width: auto;" class="status-disable text-center">
+                        <p class="m-0">No Stock</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-9">
+                    <div class="d-flex flex-row">
+                        <div class="me-2 rounded"  style="width: 30%; height: 70px">
+                            <img  class="border rounded" style="width: 100%; height: 100%; object-fit: cover" src="../../POS/pages/upload_prodImg/dog collar b.jpg" alt="prod">
+                        </div>
+                        <div style="width: 70%;">
+                            <p class="fw-bold">Dog Colar large for dogs</p>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                <div class="col-3">
+                    <p class="fw-bold text-end m-0">20 pcs</p>
+                    <div style="width: auto;" class="status-warning text-center">
+                        <p class="m-0">Critical</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-9">
+                    <div class="d-flex flex-row">
+                        <div class="me-2 rounded"  style="width: 30%; height: 70px">
+                            <img  class="border rounded" style="width: 100%; height: 100%; object-fit: cover" src="../../POS/pages/upload_prodImg/cup-chiken.jpg" alt="prod">
+                        </div>
+                        <div style="width: 70%;">
+                            <p class="fw-bold">Pet Eating Cup for pet</p>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                <div class="col-3">
+                    <p class="fw-bold text-end m-0">90 pcs</p>
+                    <div style="width: auto;" class="status-active text-center">
+                        <p class="m-0">In-Stock</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-9">
+                    <div class="d-flex flex-row">
+                        <div class="me-2 rounded"  style="width: 30%; height: 70px">
+                            <img  class="border rounded" style="width: 100%; height: 100%; object-fit: cover" src="../../POS/pages/upload_prodImg/paw roller.jpg" alt="prod">
+                        </div>
+                        <div style="width: 70%;">
+                            <p class="fw-bold">Paw Roller for dogs hair</p>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                <div class="col-3">
+                    <p class="fw-bold text-end m-0">100 pcs</p>
+                    <div style="width: auto;" class="status-active text-center">
+                        <p class="m-0">In-Stock</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <button style="border-radius: 10px; background-color: #760106; color: white" class="btn mb-2 border w-100">
+                view all
+            </button>
+            
+        </div>
---
+++ POS/pages/users_statistics.php
@@ -0,0 +1,28 @@
+<div class="border shadow p-3" style="background-color: white; border-radius: 20px;">
+    <div class="row g-3">
+        <div class="col-6 text-center col-md-3">
+            <div class="accent-a" style="cursor: pointer">
+                <h1 style="font-size: 3rem;" class="fw-bolder">100</h1>
+                <h5>Customer</h5>
+            </div>
+        </div>
+        <div class="col-6 text-center col-md-3">
+            <div class="accent-b" style="cursor: pointer">
+                <h1 style="font-size: 3rem;" class="fw-bolder">100</h1>
+                <h5>Cashier</h5>
+            </div>
+        </div>
+        <div class="col-6 text-center col-md-3">
+            <div class="accent-c" style="cursor: pointer">
+                <h1 style="font-size: 3rem;" class="fw-bolder">100</h1>
+                <h5>Rider</h5>
+            </div>
+        </div>
+        <div class="col-6 text-center col-md-3">
+            <div class="accent-d" style="cursor: pointer">
+                <h1 style="font-size: 3rem;" class="fw-bolder">100</h1>
+                <h5>Supplier</h5>
+            </div>
+        </div>
+    </div>
+</div>
