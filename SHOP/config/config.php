4e0a09569db16c751bc7ef682141ae8fd5bb9966
Fyke Lleva <floterina@gmail.com>
2024-08-11 01:54:10 +0800

    shop UI

---
+++ SHOP/config/config.php
@@ -0,0 +1,23 @@
+<?php
+// // Database credentials
+$hostname = 'localhost';      // Change this to your database hostname
+$username = 'root';   // Change this to your database username
+$password = '';   // Change this to your database password
+$database = 'u533477241_rdpos';   // Change this to your database name
+
+
+// $hostname = 'localhost';      // Change this to your database hostname
+// $username = 'u533477241_rdpos';   // Change this to your database username
+// $password = 'Rdpos2024';   // Change this to your database password
+// $database = 'u533477241_rdpos';   // Change this to your database name
+
+// Create a database connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+
+?>
---
+++ SHOP/index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHOP</title>
+    <?php include('links.php')?>
+</head>
+<body>
+    <?php include('navigation.php')?>
+    <?php include('script.php')?>
+</body>
+</html>
