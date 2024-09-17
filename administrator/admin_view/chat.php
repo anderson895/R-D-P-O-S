50eeb8c00cb67b2bc3782e22d75a722465f15767
JARP <105678913+anderson895@users.noreply.github.com>
2024-09-17 12:00:58 +0800

    back to default connection

--- administrator/admin_view/chat.php
+++ administrator/admin_view/chat.php
@@ -64,7 +64,7 @@ include "php/session_dir.php";
             <?php include "topBar/profile.php"; ?>
     </ul>
 
-    <?php include "topbar/mobilUserMenu.php"; ?>
+    <?php include "topBar/mobilUserMenu.php"; ?>
 </div>
     <?php  include "Section/sidebar.php"; ?>
 
--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$connections = mysqli_connect ("localhost","u547077750_rdpos","Rdpos2024","u547077750_rdpos");
+$connections = mysqli_connect ("localhost","root","","u533477241_rdpos");
 // $connections = mysqli_connect ("localhost","u533477241_rdpos","Rdpos2024","u533477241_rdpos");
 ?>
