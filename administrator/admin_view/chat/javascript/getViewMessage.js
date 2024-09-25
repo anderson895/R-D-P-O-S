bb6952524f199cd1f45963ef551ba305b9d12645
JARP <andersonandy046@gmail.com>
2024-09-25 12:08:29 +0800

    Update getViewMessage.js

--- administrator/admin_view/chat/javascript/getViewMessage.js
+++ administrator/admin_view/chat/javascript/getViewMessage.js
@@ -55,67 +55,7 @@ $(document).ready(function() {
                     messageHtml += '<div class="media-body flex-grow-1">';
                     messageHtml += '<div class="msg-box"><div>';
 
-                    if (message.mess_type === 'image') {
-                        messageHtml += '<div class="chat-msg-attachments">';
-                        messageHtml += '<div class="chat-attachment">';
-                        messageHtml += '<img src="../../upload_message/' + message.mess_content + '" alt="Attachment">';
-                        messageHtml += '<a href="../../upload_message/' + message.mess_content + '" class="chat-attach-download" id="downloadLink" download>';
-                        messageHtml += '<i class="fas fa-download"></i>';
-                        messageHtml += '</a>';
-                        messageHtml += '</div>';
-                        messageHtml += '</div>';
-                    } else if (message.mess_type === 'audio') {
-                        messageHtml += '<div  class="chat-msg-attachments">';
-                        messageHtml += '<div class="chat-attachment audio-attachment">';
-                        messageHtml += '<audio style="width:100px;" controls class="audio-player">';
-                        messageHtml += '<source src="../upload_message/' + message.mess_content + '" type="audio/mpeg">';
-                        messageHtml += 'Your browser does not support the audio element.';
-                        messageHtml += '</audio>';
-                        messageHtml += '<a href="../upload_message/' + message.mess_content + '" class="chat-attach-download" id="downloadLink" download>';
-                        messageHtml += '<i class="fas fa-download"></i>';
-                        messageHtml += '</a>';
-                        messageHtml += '</div>';
-                        messageHtml += '</div>';
-                    } else if (message.mess_type === 'document') {
-                       // Extracting file extension
-                       const fileName = message.mess_content.split('.')[0]; // Extract file name without extension
-                       const fileExtension = message.mess_content.split('.').pop();
-                       
-                       // Determine icon and color based on file extension
-                       let iconClass = 'fas'; // Default icon class for files
-                       let iconColor = 'black'; // Default color
-                       
-                       // Define specific icons and colors for certain file types
-                       if (fileExtension === 'pdf') {
-                           iconClass = 'fas fa-file-pdf'; // Change to PDF icon class
-                           iconColor = 'red'; // Change to the desired color for PDF files
-                       } else if (fileExtension === 'docx' || fileExtension === 'doc') {
-                           iconClass = 'fas fa-file-word'; // Change to Word document icon class
-                           iconColor = 'blue'; // Change to the desired color for Word documents
-                       }
-
-                       messageHtml += `
-                       <div class="chat-msg-attachments">
-                           <div class="chat-attachment" style="display: flex; align-items: center;">
-                               <i class="${iconClass}" style="font-size:24px; margin-right: 10px; color: ${iconColor};"></i>
-                               <p style="margin-right: 10px; ">${fileName}.${fileExtension}</p>
-                               <a href="../upload_message/${message.mess_content}" class="chat-attach-download" id="downloadLink" download>
-                               <i class="fas fa-download"></i>
-                           </a>
-                           
-                           </div>
-                       </div>
-                       `;
-                       
-
-                    } else {
-                       // console.log(message);
-
-                       messageHtml += '<i><b>' + message.acc_fname + ' ' + message.acc_lname + '</b> (' + message.acc_type + ')</i>';
-
-
-                        messageHtml += '<p >'+ message.mess_content + '</p>';
-                    }
+                   
 
                     messageHtml += '<ul class="chat-msg-info"><li>';
                     messageHtml += '<div class="chat-time"><span>' + formattedTime + '</span></div>';
