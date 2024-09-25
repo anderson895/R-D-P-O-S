e7eeb084218795eb5086d47cc5644aede7cbc327
JARP <andersonandy046@gmail.com>
2024-09-25 23:47:32 +0800

    Update mobilUserMenu.php

--- administrator/admin_view/topBar/mobilUserMenu.php
+++ administrator/admin_view/topBar/mobilUserMenu.php
@@ -4,7 +4,7 @@
 <div class="dropdown-menu dropdown-menu-right">
 <!-- <a class="dropdown-item" href="profile.php?account_id=<?=$db_acc_id?>">My Profile</a> -->
 <a class="dropdown-item" href="generalsettings.php?account_id=<?=$db_acc_id?>">Settings</a>
-<a class="dropdown-item" href="privacysettings.php?account_id=<?=$db_acc_id?>';">Privacy</a>
+<a class="dropdown-item" href="privacysettings.php?account_id=<?=$db_acc_id?>">Privacy</a>
 
 <a class="dropdown-item" href="backend/logout.php">Logout</a>
 </div>
