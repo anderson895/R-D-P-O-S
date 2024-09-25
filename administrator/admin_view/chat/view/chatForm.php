3fa40700861f234c46ca87eea63132eeb83d895f
JARP <andersonandy046@gmail.com>
2024-09-25 11:32:17 +0800

    Update chatForm.php

--- administrator/admin_view/chat/view/chatForm.php
+++ administrator/admin_view/chat/view/chatForm.php
@@ -86,11 +86,11 @@ $fullname=$acc_fname." ".$acc_lname;
 
 
 
-    <input hidden type="text" value="<?= $account_id; ?>" id="account_id">
+    <input  type="text" value="<?= $account_id; ?>" id="account_id">
     
-    <input hidden  type="text" value="<?= $session_id; ?>" id="session_id">
+    <input   type="text" value="<?= $session_id; ?>" id="session_id">
 
-    <div <?php if($_GET['account_id']=='view_all'){ echo "hidden";} ?> class="card mb-0">
+    <div class="card mb-0">
     <div class="card-header msg_head">
     <div class="d-flex bd-highlight">
     <a id="back_user_list" href="javascript:void(0)" class="back-user-list">
