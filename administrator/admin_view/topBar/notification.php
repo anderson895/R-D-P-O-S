0b05fb1fd290700395776973a3e0c124ead57b2f
JARP <andersonandy046@gmail.com>
2024-09-25 11:52:46 +0800

    Update notification.php

--- administrator/admin_view/topBar/notification.php
+++ administrator/admin_view/topBar/notification.php
@@ -22,7 +22,7 @@
 </ul>
 </div>
 <div class="topnav-dropdown-footer">
-<a href="chat.php?account_id=view_all">View all Messages</a>
+<a href="chat.php?account_id=<?=$session_id?>">View all Messages</a>
 </div>
 </div>
 </li>
