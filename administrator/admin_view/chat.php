523dd7acbd0f094ba6ff8bc300b3e6b521be18cf
JARP <andersonandy046@gmail.com>
2024-09-26 00:31:24 +0800

    Update chat.php

--- administrator/admin_view/chat.php
+++ administrator/admin_view/chat.php
@@ -8,13 +8,11 @@ include "php/session_dir.php";
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-<meta name="description" content="POS - Bootstrap Admin Template">
-<meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern,  html5, responsive">
-<meta name="author" content="Dreamguys - Bootstrap Admin Template">
-<meta name="robots" content="noindex, nofollow">
+<meta name="keywords" content="Rdpos system">
+
 <title>Messages</title>
 
-
+<link rel="shortcut icon" type="image/x-icon" href="../../upload_system/<?= $db_system_logo ?>">
 
 <link rel="stylesheet" href="assets/plugins/scrollbar/scroll.min.css">
 
