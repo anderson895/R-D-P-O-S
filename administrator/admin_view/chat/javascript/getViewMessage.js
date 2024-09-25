04a24d57f4e575d2b7055a5292fa84991b0b5354
JARP <andersonandy046@gmail.com>
2024-09-25 12:35:37 +0800

    Update getViewMessage.js

--- administrator/admin_view/chat/javascript/getViewMessage.js
+++ administrator/admin_view/chat/javascript/getViewMessage.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
             dataType: 'json',
             success: function(data) {
 
-                console.log(data);
+                // console.log(data);
                 
                 var chatBody = $('#messageList');
                 chatBody.empty();
