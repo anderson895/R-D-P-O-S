feec63f20cf2a181f22b198c2820dd95f24b1183
JARP <andersonandy046@gmail.com>
2024-09-26 00:04:59 +0800

    Update header.php

--- administrator/admin_view/topBar/header.php
+++ administrator/admin_view/topBar/header.php
@@ -1,8 +1,8 @@
 <div class="header-left active" style="background-color: rgb(113,0,0);">
     <a href="<?php if($acc_type === 'administrator'){ echo 'index.php ';}else{ echo '#';} ?>"
-       class="d-flex flex-row align-items-center justify-content-between w-100">
-        <img src="../../upload_system/<?= $db_system_logo ?>" class="img-fluid" alt="Logo" style="max-width:50px; height: auto;">
-        <span class="fs-3 fw-bold text-light ms-3"><?= $db_system_name ?></span>
+       class="d-flex flex-column flex-md-row align-items-center justify-content-center text-center w-100">
+        <img src="../../upload_system/<?= $db_system_logo ?>" class="img-fluid" alt="Logo" style="max-width:30px; height: auto;">
+        <span class="fs-3 fw-bold text-light mt-2 mt-md-0 ms-md-3"><?= $db_system_name ?></span>
     </a>
 </div>
 
