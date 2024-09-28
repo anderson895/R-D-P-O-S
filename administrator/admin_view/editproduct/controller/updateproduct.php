0d3d14874692e054f47a45d7d343591214ce62d3
JARP <andersonandy046@gmail.com>
2024-09-29 02:17:31 +0800

    Update updateproduct.php

--- administrator/admin_view/editproduct/controller/updateproduct.php
+++ administrator/admin_view/editproduct/controller/updateproduct.php
@@ -4,23 +4,27 @@ include("../../../../connection.php");
 date_default_timezone_set('Asia/Manila');
 $currentDateTime = date('Y-m-d H:i:s');
 
-$pname = $pcat = $pcritical = $pDescript = $pVouch = $pCprice = $pImg = "";
+$pname  = $pcat = $pcritical = $pDescript = $pVouch  = $pCprice = $pImg = "";
 $prod_code = $acc_id = "";
 
-// Debugging output for $_POST data
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
 
-// Sanitize and validate input
 $pname = mysqli_real_escape_string($connections, preg_replace('/[^0-9.,a-zA-Z\s]/', '', $_POST["pname"]));
+
 $pCprice = floatval(preg_replace('/[^0-9.]/', '', $_POST["pCprice"]));
 $mg = $_POST["mg"];
 $ml = $_POST["ml"];
 $g = $_POST["g"];
+
 $unitType = $_POST['unitType'];
+
+// $vatableTogler = $_POST["vatableTogler"];
 $discountableTogler = $_POST["discountableTogler"];
 $SellOnlineTogler = $_POST["SellOnlineTogler"];
+
+
 $pcat = intval(preg_replace('/[^0-9]/', '', $_POST["pcat"]));
 $pcritical = intval(preg_replace('/[^0-9]/', '', $_POST["pcritical"]));
 $pDescript = mysqli_real_escape_string($connections, preg_replace('/[^0-9.,a-zA-Z\s]/', '', $_POST["pDescript"]));
@@ -28,136 +32,143 @@ $pVouch = preg_replace('/[^0-9.,]/', '', $_POST["pVouch"]);
 $prod_code = preg_replace('/[^0-9.,a-zA-Z]/', '', $_POST["prod_code"]);
 $acc_id = preg_replace('/[^0-9]/', '', $_POST["acc_id"]);
 
-// Handle file upload
+
 if ($_FILES['pImg']['error'] === UPLOAD_ERR_OK) {
     $imagePath = '../../../../upload_prodImg';
-    $fileExtension = strtolower(pathinfo($_FILES['pImg']['name'], PATHINFO_EXTENSION));
-    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif']; // Allowed file types
+    $fileExtension = pathinfo($_FILES['pImg']['name'], PATHINFO_EXTENSION);
+    $uniqueFilename = uniqid() . '.' . $fileExtension;
 
-    if (in_array($fileExtension, $allowedExtensions)) {
-        $uniqueFilename = uniqid() . '.' . $fileExtension;
-        $targetFile = $imagePath . '/' . $uniqueFilename;
+    $targetFile = $imagePath . '/' . $uniqueFilename;
 
-        if (move_uploaded_file($_FILES['pImg']['tmp_name'], $targetFile)) {
-            $pImg = $uniqueFilename;
-        } else {
-            echo "Error uploading file.";
-            exit;
-        }
+    // Check if a file with the same name exists
+    if (file_exists($targetFile)) {
+        // If it exists, delete the existing file
+        unlink($targetFile);
+    }
+
+    // Move the uploaded file to the target directory
+    if (move_uploaded_file($_FILES['pImg']['tmp_name'], $targetFile)) {
+        $pImg = $uniqueFilename; // Update the $pImg variable with the new file name
     } else {
-        echo "Invalid file type. Only JPG, JPEG, PNG, and GIF files are allowed.";
-        exit;
+        echo "Error moving uploaded file.";
     }
 }
 
-// Fetch existing product data
-$query = "SELECT * FROM product 
-          LEFT JOIN category ON category.category_id = product.prod_category_id
-          LEFT JOIN voucher ON voucher.voucher_id = product.prod_voucher_id
-          WHERE prod_code = ?";
-$stmt = mysqli_prepare($connections, $query);
-mysqli_stmt_bind_param($stmt, 's', $prod_code);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-$row = mysqli_fetch_assoc($result);
 
-if (!$row) {
-    echo "Error: Product not found.";
-    exit;
-}
-
-// Store original values for comparison
+$get_record = mysqli_query($connections, "SELECT *
+     FROM product
+     LEFT JOIN category ON category.category_id = product.prod_category_id
+     LEFT JOIN voucher ON voucher.voucher_id = product.prod_voucher_id
+     WHERE prod_code = '$prod_code' ");
+$row = mysqli_fetch_assoc($get_record);
+$db_prod_id = $row["prod_id"];
+$db_prod_code = $row["prod_code"];
 $db_prod_name = $row["prod_name"];
+
 $db_prod_currprice = $row["prod_currprice"];
 $db_prod_critical = $row["prod_critical"];
 $db_prod_description = $row["prod_description"];
 $db_prod_image = $row["prod_image"];
+$db_prod_added = $row["prod_added"];
+
+
+$db_prod_category_id = $row["prod_category_id"];
+$db_prod_voucher_id = $row["prod_voucher_id"];
+
+
 $db_category_name = $row["category_name"];
 $db_voucher_name = $row["voucher_name"];
 
-// Update product data
+
+
 if ($prod_code > 0) {
-    $updateQuery = "UPDATE product SET 
-                    prod_name = ?, 
-                    prod_currprice = ?, 
-                    prod_mg = ?, 
-                    prod_ml = ?, 
-                    prod_g = ?, 
-                    prod_category_id = ?, 
-                    prod_critical = ?, 
-                    prod_description = ?, 
-                    prod_voucher_id = ?, 
-                    prod_edit = ?, 
-                    prod_sell_onlline = ?, 
-                    unit_type = ?";
+    $query = "UPDATE product SET 
+                  prod_name = '$pname',
+                  prod_currprice = '$pCprice',
+                  prod_mg = '$mg',
+                  prod_ml = '$ml',
+                  prod_g = '$g',
+                  prod_category_id = '$pcat',
+                  prod_critical = '$pcritical',
+                  prod_description = '$pDescript',
+                  prod_voucher_id = '$pVouch',
+                  prod_edit = '$currentDateTime',
+                  prod_sell_onlline = '$SellOnlineTogler',
+                  unit_type = '$unitType'
+                  ";
 
     if ($pImg) {
-        $updateQuery .= ", prod_image = ?";
+        $query .= ", prod_image = '$pImg'";
     }
 
-    $updateQuery .= " WHERE prod_code = ?";
-    $stmtUpdate = mysqli_prepare($connections, $updateQuery);
+    $query .= " WHERE prod_code = '$prod_code'";
 
-    if ($pImg) {
-        mysqli_stmt_bind_param($stmtUpdate, 'sdsddiiissssi', $pname, $pCprice, $mg, $ml, $g, $pcat, $pcritical, $pDescript, $pVouch, $currentDateTime, $SellOnlineTogler, $unitType, $pImg, $prod_code);
+    if (mysqli_query($connections, $query)) {
+
+
+
+        $get_record = mysqli_query($connections, "SELECT *
+            FROM product
+            LEFT JOIN category ON category.category_id = product.prod_category_id
+            LEFT JOIN voucher ON voucher.voucher_id = product.prod_voucher_id
+            WHERE prod_code = '$prod_code' ");
+        $row = mysqli_fetch_assoc($get_record);
+        $new_db_prod_id = $row["prod_id"];
+        $new_db_prod_code = $row["prod_code"];
+        $new_db_prod_name = $row["prod_name"];
+        $new_db_prod_currprice = $row["prod_currprice"];
+        $new_db_prod_critical = $row["prod_critical"];
+        $new_db_prod_description = $row["prod_description"];
+        $new_db_prod_image = $row["prod_image"];
+        $new_db_prod_added = $row["prod_added"];
+
+
+        $new_db_prod_category_id = $row["prod_category_id"];
+        $new_db_prod_voucher_id = $row["prod_voucher_id"];
+
+
+        $new_db_category_name = $row["category_name"];
+        $new_db_voucher_name = $row["voucher_name"];
+
+
+        if ($db_prod_name != $new_db_prod_name) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed to $new_db_prod_name ', '$currentDateTime','product','$prod_code')");
+        }
+
+        if ($db_prod_currprice != $new_db_prod_currprice) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed current price: $db_prod_currprice to $new_db_prod_currprice', '$currentDateTime','product','$prod_code')");
+        }
+        if ($db_prod_critical != $new_db_prod_critical) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed critical level $db_prod_critical to $new_db_prod_critical', '$currentDateTime','product','$prod_code')");
+        }
+
+        if ($db_prod_description != $new_db_prod_description) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed product description $db_prod_description to $pDescript', '$currentDateTime','product','$prod_code')");
+        }
+        if ($db_prod_image != $new_db_prod_image) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed product image', '$currentDateTime','product','$prod_code')");
+        }
+
+
+        if ($db_category_name != $new_db_category_name) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed category $db_category_name to $new_db_category_name', '$currentDateTime','product','$prod_code')");
+        }
+        if ($db_voucher_name != $new_db_voucher_name) {
+            mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date,act_table,act_collumn_id) 
+                        VALUES('$acc_id', '$db_prod_name changed voucher $db_voucher_name to $new_db_voucher_name', '$currentDateTime','product','$prod_code')");
+        }
+
+
+        exit;
     } else {
-        mysqli_stmt_bind_param($stmtUpdate, 'sdsddiiissss', $pname, $pCprice, $mg, $ml, $g, $pcat, $pcritical, $pDescript, $pVouch, $currentDateTime, $SellOnlineTogler, $unitType, $prod_code);
-    }
 
-    if (mysqli_stmt_execute($stmtUpdate)) {
-        // Fetch updated product data
-        $result = mysqli_query($connections, "SELECT * FROM product 
-                                              LEFT JOIN category ON category.category_id = product.prod_category_id
-                                              LEFT JOIN voucher ON voucher.voucher_id = product.prod_voucher_id
-                                              WHERE prod_code = '$prod_code'");
-        $row = mysqli_fetch_assoc($result);
-
-        // Log changes if values have changed
-        logChanges($connections, $row, $db_prod_name, $db_prod_currprice, $db_prod_critical, $db_prod_description, $db_prod_image, $db_category_name, $db_voucher_name, $acc_id, $prod_code, $currentDateTime);
-
-        echo "Product updated successfully.";
-    } else {
         echo "Error updating product: " . mysqli_error($connections);
+        exit;
     }
 }
-
-function logChanges($connections, $newRow, $oldName, $oldPrice, $oldCritical, $oldDescription, $oldImage, $oldCategory, $oldVoucher, $accountId, $productCode, $currentDateTime) {
-    // Compare and log changes
-    if ($oldName != $newRow["prod_name"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed to {$newRow['prod_name']}', '$currentDateTime', 'product', '$productCode')");
-    }
-
-    if ($oldPrice != $newRow["prod_currprice"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed current price: $oldPrice to {$newRow['prod_currprice']}', '$currentDateTime', 'product', '$productCode')");
-    }
-
-    if ($oldCritical != $newRow["prod_critical"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed critical level $oldCritical to {$newRow['prod_critical']}', '$currentDateTime', 'product', '$productCode')");
-    }
-
-    if ($oldDescription != $newRow["prod_description"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed product description: $oldDescription to {$newRow['prod_description']}', '$currentDateTime', 'product', '$productCode')");
-    }
-
-    if ($oldImage != $newRow["prod_image"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed product image', '$currentDateTime', 'product', '$productCode')");
-    }
-
-    if ($oldCategory != $newRow["category_name"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed category $oldCategory to {$newRow['category_name']}', '$currentDateTime', 'product', '$productCode')");
-    }
-
-    if ($oldVoucher != $newRow["voucher_name"]) {
-        mysqli_query($connections, "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-                                     VALUES('$accountId', '$oldName changed voucher $oldVoucher to {$newRow['voucher_name']}', '$currentDateTime', 'product', '$productCode')");
-    }
-}
-
-mysqli_close($connections);
-?>
