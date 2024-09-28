00d0716952f1d6a6ddd569cb1c30c5a6d659c4f8
JARP <andersonandy046@gmail.com>
2024-09-29 03:05:51 +0800

    Update footer.php

--- new-customer-website/components/footer.php
+++ new-customer-website/components/footer.php
@@ -314,7 +314,7 @@
                 </button>
                 <div class="d-flex justify-content-center">
                     <div class="spinner-border" role="status">
-                        <span class="sr-only">Loading...</span>
+                        <span class="sr-only"></span>
                     </div>
                 </div>
 
