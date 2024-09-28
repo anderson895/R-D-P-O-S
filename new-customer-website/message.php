ad7d676cfd5d76232fab17e5dbdc84388a885c60
JARP <andersonandy046@gmail.com>
2024-09-29 04:02:35 +0800

    Update message.php

--- new-customer-website/message.php
+++ new-customer-website/message.php
@@ -1,6 +1,8 @@
 <?php
 include('components/header.php');
 ?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="css/chat.css">
 <link rel="stylesheet" href="../administrator/admin_view/assets/plugins/alertify/alertify.min.css">
 
