29f61bd1c1c75844149d451c61a414282e286e51
JARP <andersonandy046@gmail.com>
2024-09-29 01:03:36 +0800

    Update get-cart-items.php

--- new-customer-website/backend/end-points/get-cart-items.php
+++ new-customer-website/backend/end-points/get-cart-items.php
@@ -91,10 +91,16 @@ if ($getCartItemsPhotos->num_rows > 0){
         <!-- Item Details -->
         <div class="col-md-9">
             <div class="item-details border p-3 rounded-3 shadow-sm">
-            <input type="checkbox" class="form-check-input me-2 cartSelect" data-id="<?= $cartItem['prod_id'] ?>" data-image="<?= $cartItem['prod_image'] ?>" data-name="<?= $productName ?>" data-price="<?= $cartItem['prod_currprice'] ?>" data-unittype="<?= $cartItem['unit_type'] ?>" data-amount="<?= $itemAmount ?>" data-stock="<?= $currentStock ?>" data-inputqty="<?= $cartItem['qty'] ?>" data-itemvat="<?= $vatPerItem ?>" style="width: 30px; height: 30px;">
-                <h4 class="fw-bold"><?= $productName ?></h4>
-                <button class="btn btn-danger btn-sm d-flex align-items-center ms-2 btnDeleteCartItem" data-id="<?= $cartItem['cart_id'] ?>"><i class="bi bi-trash3-fill"></i> </button>
-               
+            <div class="d-flex align-items-center mb-3">
+                <input type="checkbox" class="form-check-input me-2 cartSelect" data-id="<?= $cartItem['prod_id'] ?>" data-image="<?= $cartItem['prod_image'] ?>" data-name="<?= $productName ?>" data-price="<?= $cartItem['prod_currprice'] ?>" data-unittype="<?= $cartItem['unit_type'] ?>" data-amount="<?= $itemAmount ?>" data-stock="<?= $currentStock ?>" data-inputqty="<?= $cartItem['qty'] ?>" data-itemvat="<?= $vatPerItem ?>" style="width: 30px; height: 30px;">
+                
+                <h4 class="fw-bold me-2"><?= $productName ?></h4>
+                
+                <button class="btn btn-danger btn-sm d-flex align-items-center" data-id="<?= $cartItem['cart_id'] ?>">
+                    <i class="bi bi-trash3-fill"></i>
+                </button>
+            </div>
+
                 <div class="mb-3">
                     <label for="product-description" class="form-label">Description</label>
                     <textarea id="product-description" class="form-control" style="height: 150px" readonly><?= $cartItem['prod_description'] ?></textarea>
