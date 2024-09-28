5d39848604a392e1f2070ba93723b0da7dac01bd
JARP <andersonandy046@gmail.com>
2024-09-28 12:45:07 +0800

    Update signup.php

--- Main/view/Signup/signup.php
+++ Main/view/Signup/signup.php
@@ -96,12 +96,12 @@
                         </div>
 
                         <div class="form-login">
-                        <div class="form-check" style="display: flex; align-items: center;">
-            <input type="checkbox" id="agreeTermsCheckbox" style="width: 16px; height: 16px; margin-right: 5px;" required>
-            <label for="agreeTermsCheckbox" style="margin: 0;">
-                <a href="#" id="termsLink" style="text-decoration: underline;">I agree to the Terms and Conditions</a>
-            </label>
-</div>
+                            <div class="form-check" style="display: flex; align-items: center;">
+                                    <input type="checkbox" id="agreeTermsCheckbox" style="width: 16px; height: 16px; margin-right: 5px;" required>
+                                    <label for="agreeTermsCheckbox" style="margin: 0;">
+                                        <a href="#" id="termsLink" style="text-decoration: underline;">I agree to the Terms and Conditions</a>
+                                    </label>
+                            </div>
 
 
 
