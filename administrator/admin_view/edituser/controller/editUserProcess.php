94647cdd6288c18dc563c1d671deee32080fec3b
JARP <andersonandy046@gmail.com>
2024-09-29 01:46:41 +0800

    Update editUserProcess.php

--- administrator/admin_view/edituser/controller/editUserProcess.php
+++ administrator/admin_view/edituser/controller/editUserProcess.php
@@ -1,7 +1,6 @@
 <?php
 include("../../../../connection.php");
 
-
 $acc_code = $_POST["acc_code"];
 date_default_timezone_set('Asia/Manila');
 $currentDateTime = date('Y-m-d H:i:s');
@@ -15,9 +14,7 @@ $password = $_POST["password"];
 $accountType = $_POST["accountType"];
 $acc_id = $_POST["acc_id"];
 
-
-
-  print_r($_POST);
+print_r($_POST);
 
 $get_record = mysqli_query($connections, "SELECT * FROM account WHERE acc_code = '$acc_code'");
 $row = mysqli_fetch_assoc($get_record);
@@ -32,9 +29,9 @@ if ($row) {
     $db_acc_type = $row["acc_type"];
     $db_acc_contact = $row["acc_contact"];
 
-
     $userImage = $db_userImage; // Initialize userImage with the existing value
 
+    // Handle file upload
     if ($_FILES && $_FILES['userImage']['error'] === UPLOAD_ERR_OK) {
         $imagePath = '../../../../upload_img';
         $fileExtension = pathinfo($_FILES['userImage']['name'], PATHINFO_EXTENSION);
@@ -42,6 +39,13 @@ if ($row) {
         $targetFile = $imagePath . '/' . $uniqueFilename;
 
         if (is_uploaded_file($_FILES['userImage']['tmp_name']) && move_uploaded_file($_FILES['userImage']['tmp_name'], $targetFile)) {
+            // If there was an old image, delete it
+            if ($db_userImage) {
+                $existingImagePath = $imagePath . '/' . $db_userImage;
+                if (file_exists($existingImagePath)) {
+                    unlink($existingImagePath); // Delete the existing image file
+                }
+            }
             $userImage = mysqli_real_escape_string($connections, $uniqueFilename);
         } else {
             $errorDetails = error_get_last();
@@ -50,12 +54,10 @@ if ($row) {
         }
     }
 
-    if($password){
-  
-    
-    $hashedPassword = hash('sha256', $password);
-    
-    $updateQuery = "UPDATE account SET 
+    // Prepare the update query based on whether the password is provided
+    if ($password) {
+        $hashedPassword = hash('sha256', $password);
+        $updateQuery = "UPDATE account SET 
             acc_created = '$currentDateTime',
             acc_username = '$username',
             acc_password = '$hashedPassword',
@@ -66,68 +68,43 @@ if ($row) {
             acc_contact = '$phone',
             emp_image = '$userImage'
         WHERE acc_code = '$acc_code'";
-        
-      
-
-
-
-    }else{
+    } else {
         $updateQuery = "UPDATE account SET 
-        acc_created = '$currentDateTime',
-        acc_username = '$username',
-        acc_fname = '$fname',
-        acc_lname = '$lname',
-        acc_type = '$accountType',
-        acc_email = '$email',
-        acc_contact = '$phone',
-        emp_image = '$userImage'
-    WHERE acc_code = '$acc_code'";
-
+            acc_created = '$currentDateTime',
+            acc_username = '$username',
+            acc_fname = '$fname',
+            acc_lname = '$lname',
+            acc_type = '$accountType',
+            acc_email = '$email',
+            acc_contact = '$phone',
+            emp_image = '$userImage'
+        WHERE acc_code = '$acc_code'";
     }
-    
-    
-    
-    
-    
-    
-    
-   
 
     // Log the activity
-    if ($userImage != $db_userImage 
-    || $db_acc_username != $username
-    || $fname != $db_acc_fname 
-    || $lname!=$db_acc_lname 
-    || $email !=$db_acc_email 
-    || $phone != $db_acc_contact
-    || $userImage != $db_userImage
-    ) {
-            $logQuery = "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-            VALUES('$acc_id', 'Update $db_acc_fname $db_acc_lname`s information', '$currentDateTime', 'account', '$db_acc_id')";
-        
-                if (mysqli_query($connections, $logQuery) && mysqli_query($connections, $updateQuery)) {
-                    echo "Account updated successfully.";
-                } else {
-                    echo "Error: " . mysqli_error($connections);
-                }
+    if ($userImage !== $db_userImage 
+    || $db_acc_username !== $username
+    || $fname !== $db_acc_fname 
+    || $lname !== $db_acc_lname 
+    || $email !== $db_acc_email 
+    || $phone !== $db_acc_contact
+    || $accountType !== $db_acc_type) {
 
-    }else if($accountType != $db_acc_type){
         $logQuery = "INSERT INTO users_log(act_account_id, act_activity, act_date, act_table, act_collumn_id) 
-        VALUES('$acc_id', 'Update $db_acc_fname $db_acc_lname`s account type from $db_acc_type changed to `$accountType` ', '$currentDateTime', 'account', '$db_acc_id')";
-                if (mysqli_query($connections, $logQuery) && mysqli_query($connections, $updateQuery)) {
-                                    echo "Account updated successfully.";
-                                } else {
-                                    echo "Error: " . mysqli_error($connections);
-                                }
-    }else{
+        VALUES('$acc_id', 'Update $db_acc_fname $db_acc_lname`s information', '$currentDateTime', 'account', '$db_acc_id')";
         
-            if (mysqli_query($connections, $updateQuery)) {
-                    echo "Account updated successfully password only.";
-                } else {
-                    echo "Error: " . mysqli_error($connections);
-                }
+        if (mysqli_query($connections, $logQuery) && mysqli_query($connections, $updateQuery)) {
+            echo "Account updated successfully.";
+        } else {
+            echo "Error: " . mysqli_error($connections);
+        }
+    } else {
+        if (mysqli_query($connections, $updateQuery)) {
+            echo "Account updated successfully. No changes made to the image or information.";
+        } else {
+            echo "Error: " . mysqli_error($connections);
+        }
     }
-
 } else {
     echo "Error: Account with acc_code '$acc_code' not found.";
 }
