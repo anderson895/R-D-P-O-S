fe41c52d2b7720b5c52428357539e299e424051c
JARP <andersonandy046@gmail.com>
2024-09-28 23:25:33 +0800

    Update signup.php

--- Main/view/Signup/signup.php
+++ Main/view/Signup/signup.php
@@ -25,7 +25,6 @@
 </style>
 
 <body class="account-page">
-
 <div class="main-wrapper">
     <div class="account-content">
         <div class="login-wrapper">
@@ -38,90 +37,65 @@
 
                         <div class="row">
                             <div class="col-md-6">
-                                <div class="form-login">
-                                    <label>First Name</label>
-                                    <div class="form-addons">
-                                        <input type="text" placeholder="Enter your first name" name='fname' id="fname" value="">
-                                        <div style="color:red;" id="fnameError"></div>
-                                        <img src="view/Signup/assets/img/icons/users1.svg" alt="img">
-                                    </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="fname" name="fname" value="">
+                                    <label for="fname">First Name</label>
+                                    <div style="color:red;" id="fnameError"></div>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <div class="form-login">
-                                    <label>Last Name</label>
-                                    <div class="form-addons">
-                                        <input type="text" placeholder="Enter your last name" name='lname' id="lname" value="">
-                                        <div style="color:red;" id="lnameError"></div>
-                                        <img src="view/Signup/assets/img/icons/users1.svg" alt="img">
-                                    </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="lname" name="lname" value="">
+                                    <label for="lname">Last Name</label>
+                                    <div style="color:red;" id="lnameError"></div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-login">
+                        <div class="form-floating mb-3">
+                            <input type="date" class="form-control" id="birthdate" name="bday" value="2006-01-01" required>
                             <label for="birthdate">Birthday</label>
-                            <div class="form-addons">
-                                <input name='bday' value="2006-01-01" required type="date" id="birthdate">
-                                <div style="color:red;" id="birthdateError"></div>
-                            </div>
+                            <div style="color:red;" id="birthdateError"></div>
                         </div>
 
-                        <div class="form-login">
-                            <label>Username</label>
-                            <div class="form-addons">
-                                <input required type="text" placeholder="Username" name='username' id="username" value="">
-                                <div style="color:red;" id="usernameLengthError"></div>
-                                <div style="color:red;" id="usernameError"></div>
-                                <img src="view/Signup/assets/img/icons/mail.svg" alt="img">
-                            </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="username" name="username" value="" required>
+                            <label for="username">Username</label>
+                            <div style="color:red;" id="usernameLengthError"></div>
+                            <div style="color:red;" id="usernameError"></div>
                         </div>
 
-                        <div class="form-login">
-                            <label>Email</label>
-                            <div class="form-addons">
-                                <input required type="email" placeholder="Email" name='email' id="email" value="">
-                                <div style="color:red;" id="emailError"></div>
-                                <img src="view/Signup/assets/img/icons/mail.svg" alt="img">
-                            </div>
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="email" name="email" value="" required>
+                            <label for="email">Email</label>
+                            <div style="color:red;" id="emailError"></div>
                         </div>
 
-                        <div class="form-login">
-                            <label>Contact Number</label>
-                            <div class="form-addons">
-                                <input required type="number" placeholder="Contact Number" name='contact' id="contact" value="">
-                                <div style="color:red;" id="contactError"></div>
-                                <img src="view/Signup/assets/img/icons/mail.svg" alt="img">
-                            </div>
+                        <div class="form-floating mb-3">
+                            <input type="number" class="form-control" id="contact" name="contact" value="" required>
+                            <label for="contact">Contact Number</label>
+                            <div style="color:red;" id="contactError"></div>
                         </div>
 
-                        <div class="form-login">
-                            <label>Password</label>
-                            <div class="pass-group">
-                                <input class="pass-input" name='pass' required type="password" placeholder="Password" id="password" value="">
-                            </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="password" name="pass" value="" required>
+                            <label for="password">Password</label>
                         </div>
 
-                        <div class="form-login">
-                            <label>Confirm Password</label>
-                            <div class="pass-group">
-                                <input class="pass-input" name='cpass' required type="password" placeholder="Confirm Password" id="confirmPassword" value="">
-                                <div class="error-message" style="color:red;" id="passwordError"></div>
-                                <div class="error-message" style="color:red;" id="passwordMatchError"></div>
-                            </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="confirmPassword" name="cpass" value="" required>
+                            <label for="confirmPassword">Confirm Password</label>
+                            <div class="error-message" style="color:red;" id="passwordError"></div>
+                            <div class="error-message" style="color:red;" id="passwordMatchError"></div>
                         </div>
 
                         <div class="form-login">
                             <div class="form-check" style="display: flex; align-items: center;">
-                                    <input type="checkbox" id="agreeTermsCheckbox" style="width: 16px; height: 16px; margin-right: 5px;" required>
-                                    <label for="agreeTermsCheckbox" style="margin: 0;">
-                                        <a href="#" id="termsLink" style="text-decoration: underline;">I agree to the Terms and Conditions</a>
-                                    </label>
+                                <input type="checkbox" id="agreeTermsCheckbox" style="width: 16px; height: 16px; margin-right: 5px;" required>
+                                <label for="agreeTermsCheckbox" style="margin: 0;">
+                                    <a href="#" id="termsLink" style="text-decoration: underline;">I agree to the Terms and Conditions</a>
+                                </label>
                             </div>
-
-
-
-
 
                             <button class="btn btn-login" type="submit" id="submitButton" disabled>Create Account</button>
                             <div class="text-center" id="loadingSpinner"></div>
@@ -141,6 +115,7 @@
         </div>
     </div>
 </div>
+
 
 <!-- Modal for Terms and Conditions -->
 <div class="modal fade" id="termsModal" tabindex="-1" role="dialog" aria-labelledby="termsModalLabel" aria-hidden="true">
