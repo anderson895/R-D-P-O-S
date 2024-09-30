cc25d3972f3e577e3a2feb2872e6f7de17bf955e
JARP <andersonandy046@gmail.com>
2024-09-30 16:58:19 +0800

    Update add_stocks.php

--- administrator/admin_view/stock_in/view/add_stocks.php
+++ administrator/admin_view/stock_in/view/add_stocks.php
@@ -8,7 +8,7 @@ if (!empty($_POST["supplier_code"]) && !empty($_POST["invoice_no"])) {
     $supplier_code = $_POST["supplier_code"];
     $invoice_no = $_POST["invoice_no"];
 } else {
-    header('Location: stock_in.php');
+    header('Location: ../stock_in.php');
     exit(); // It's good practice to exit after a redirect
 }
 
