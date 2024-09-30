97ba0b1a84f8ace3fd5b61d39e688dc70e81ce15
JARP <andersonandy046@gmail.com>
2024-09-30 16:28:15 +0800

    Update index.php

--- POS/pages/index.php
+++ POS/pages/index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['acc_id'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <link rel="icon" href="../upload_system/<?=$db_system_logo?>" type="image/x-icon">
+    <link rel="icon" href="../../upload_system/<?=$db_system_logo?>" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
