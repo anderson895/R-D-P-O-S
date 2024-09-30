12918424c7f4a1ab963e89e421658821cae1b5c8
JARP <andersonandy046@gmail.com>
2024-09-30 17:13:49 +0800

    Update addproductForm.php

--- administrator/admin_view/addproduct/view/addproductForm.php
+++ administrator/admin_view/addproduct/view/addproductForm.php
@@ -13,29 +13,30 @@
         <div class="card-body">
           <div class="row">
 
-            <div class="col-lg-4 col-sm-6 col-12">
-              <div class="form-floating mb-3">
-                <input type="text" class="form-control" placeholder="Product name" name="pname" id="pname">
-                <label for="pname">Product name</label>
+
+
+            <div class="col-lg-4 col-sm-6 col-12=">
+              <div class="form-group col-12">
+                <label for="pcat">Product name</label>
+                <input type="text" placeholder="Product name" name="pname" id="pname">
                 <div style="display:none;" class="alert alert-danger" id="errorPname"></div>
               </div>
             </div>
 
 
-            <div class="col-lg-4 col-sm-6 col-12">
-              <div class="form-floating mb-3 text-center">
-                <input type="text" class="form-control" placeholder="Critical Level" name="pcritical" id="pcritical">
-                <label for="pcritical">Critical level</label>
-                <div class="alert alert-danger" id="criticalError"></div>
-              </div>
-            </div>
+
+
+
+
+
 
 
             <div class="col-lg-4 col-sm-6 col-12">
-              <div class="form-group text-center">
-                <div class="form-floating">
-                  <select class="form-control" name="pcat" id="pcat" aria-label="Floating label select example">
-                    <option value="" disabled selected>Choose Category</option>
+              <div class="text-center">
+                <div class="form-group">
+                  <label for="pcat">Category</label>
+                  <select class="form-control" name="pcat" id="pcat">
+                    <option>Choose Category</option>
                     <?php
                     $view_query = mysqli_query($connections, "SELECT * from category where category_status='1' ");
 
@@ -46,32 +47,54 @@
                       <option value='<?= $category_id ?>'><?= $category_name ?></option>
                     <?php } ?>
                   </select>
-                  <label for="pcat" class="form-label">Category</label>
+                  <div style="display:none;" class="alert alert-danger" id="categoryError"></div>
                 </div>
-                <div style="display:none;" class="alert alert-danger" id="categoryError"></div>
               </div>
             </div>
 
 
-           
+
+            <div class="col-lg-4 col-sm-6 col-12">
+              <div class="text-center">
+                <div class="form-group">
+                  <label for="pcritical">Critical level</label>
+                  <input type="text" class="form-control" placeholder="Critical Level" name="pcritical" id="pcritical">
+                  <div class="alert alert-danger " id="criticalError"></div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
             <div class="col-lg-12">
-              <div class="form-floating mb-3">
-                <textarea class="form-control" name='pDescript' id="pDescript" placeholder="Description"></textarea>
-                <label for="pDescript">Description</label>
+              <div class="form-group">
+                <label>Description</label>
+                <textarea class="form-control" name='pDescript' id="pDescript"></textarea>
                 <div style="display:none;" class="alert alert-danger" id="descriptionError"></div>
               </div>
             </div>
 
+
+
+
+
+
             <div class="col-lg-6 col-sm-6 col-12">
-              <div class="form-floating mb-3">
-                <input type="text" class="form-control" placeholder="Current price" name="pCprice" id="pCprice">
-                <label for="pCprice">Current price</label>
+              <div class="form-group">
+                <label>Current price</label>
+                <input type="text" placeholder="Current price" name="pCprice" id="pCprice">
                 <div style="display:none;" class="alert alert-danger" id="CpriceError"></div>
               </div>
             </div>
 
             <div class="col-lg-6 col-sm-6 col-12">
-              <div class="form-group mb-3">
+              <div class="form-group">
                 <label>Unit Type</label>
                 <select class="form-control" name="unitType" id="unitType">
                   <option>Choose Unit type</option>
@@ -82,50 +105,53 @@
               </div>
             </div>
 
+
+
             <div class="col-lg-12">
-            <div class="form-group mb-3">
-              <label style="text-align: left;">Product Image</label>
-              <div class="image-upload form-floating">
-                <input type="file" name="pImg" id="pImg" class="form-control" placeholder=" " aria-label="Product Image">
-                <label for="pImg" class="form-label">Choose file</label>
-                <div class="image-uploads">
-                  <img src="assets/img/icons/upload.svg" alt="img">
-                  <h4 style="text-align: left;">Drag and drop a file to upload</h4>
-                  <div style="display:none; text-align: left;" class="alert alert-danger" id="pImgError"></div>
+              <div class="form-group">
+                <label style="text-align: left;"> Product Image</label>
+                <div class="image-upload">
+                  <input type="file" name="pImg" id="pImg">
+                  <div class="image-uploads">
+                    <img src="assets/img/icons/upload.svg" alt="img">
+                    <h4 style="text-align: left;">Drag and drop a file to upload</h4>
+                    <div style="display:none; text-align: left;" class="alert alert-danger" id="pImgError"></div>
+                  </div>
                 </div>
               </div>
+              <img id="productImagePreview" src="#" alt="Product Image Preview" style="display: none; max-width: 200px; max-height: 200px;">
             </div>
-            <img id="productImagePreview" src="#" alt="Product Image Preview" style="display: none; max-width: 200px; max-height: 200px;">
-          </div>
+
+
 
 
             <div class="col-lg-12 col-sm-6 col-12 mt-4">
               <div class="row">
-                <div class="form-group col-4 mb-3">
-                  <div class="form-floating">
-                    <input type="number" class="form-control" id="ml" placeholder="0" min="0" max="1000">
-                    <label for="ml">ml</label>
-                    <div style="display:none;" class="alert alert-danger" id="errorMl"></div>
-                  </div>
+               
+
+                <div class="form-group col-4">
+                  <label>ml</label>
+                  <input type="number" class="form-control" placeholder="0" id="ml" min="0" max="1000">
+                  <div style="display:none;" class="alert alert-danger" id="errorMl"></div>
                 </div>
 
-                <div class="form-group col-4 mb-3">
-                  <div class="form-floating">
-                    <input type="number" class="form-control" id="g" placeholder="0" min="0" max="1000">
-                    <label for="g">g</label>
-                    <div style="display:none;" class="alert alert-danger" id="errorG"></div>
-                  </div>
+                <div class="form-group col-4">
+                  <label>g</label>
+                  <input type="number" class="form-control" placeholder="0" id="g" min="0" max="1000">
+                  <div style="display:none;" class="alert alert-danger" id="errorG"></div>
                 </div>
 
-                <div class="form-group col-4 mb-3">
-                  <div class="form-floating">
-                    <input type="number" class="form-control" id="mg" placeholder="0" min="0" max="1000">
-                    <label for="mg">mg</label>
-                    <div style="display:none;" class="alert alert-danger" id="errorMg"></div>
-                  </div>
+                <div class="form-group col-4">
+                  <label>mg</label>
+                  <input type="number" class="form-control" placeholder="0" id="mg" min="0" max="1000">
+                  <div style="display:none;" class="alert alert-danger" id="errorMg"></div>
                 </div>
               </div>
             </div>
+
+
+
+
 
 
             <div class="col-lg-4 col-sm-6 col-12 mt-4">
@@ -139,6 +165,8 @@
               </div>
             </div>
 
+
+
             <div class="col-lg-12 col-sm-6 col-12 mb-4">
               <label>Sell Online</label>
               <div class="status-toggle d-flex justify-content-between align-items-center">
@@ -147,6 +175,10 @@
               </div>
             </div>
 
+
+
+
+
             <div class="col-lg-12 col-sm-6 col-12">
               <canvas id="imageCanvas" style="display: none;"></canvas>
               <br><br>
@@ -154,15 +186,16 @@
                 <div id="loadingSpinner"></div>
                 <button type="submit" class="btn btn-submit me-2" name="btnSubmit" disabled id='btnSubmit'>Submit</button>
                 <button class="btn btn-cancel" id="backBtn" onclick="window.location.href='productlist.php'">Back</button>
+
               </div>
+
+
             </div>
 
           </div>
         </div>
-      </div>
-    </div>
-</form>
 
+</form>
 
 
 <script>
