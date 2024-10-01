10ca8f03a354ae754462a38b216f19027e835c63
JARP <andersonandy046@gmail.com>
2024-10-01 18:21:45 +0800

    Update header.php

--- administrator/new-orders-view/components/header.php
+++ administrator/new-orders-view/components/header.php
@@ -46,14 +46,54 @@ if (isset($_SESSION['acc_id'])) {
     <div class="alert alert-success"></div>
     <div class="alert alert-danger"></div>
     <nav class="d-flex justify-content-between align-items-center p-3 pt-0 pb-0 top-nav" style="background-color:rgb(131, 0, 0);">
-        <a href="orders.php">
-            <img src="../../upload_system/logo.png">
-        </a>
-        <div>
-            <?= ($user['acc_type'] == 'administrator') ? '<a href="../admin_view/index.php" class="text-light" style="text-decoration: none; margin-right: 20px">Dashboard</a>' : '' ?>
-            <button id="btnProfile" class="btn-profile">
-                <?= ($user['emp_image'] != '') ? '<img src="../../upload_img/' . $user['emp_image'] . '">' :  '<i class="bi bi-person-fill"></i>' ?>
+    <a href="orders.php">
+        <img src="../../upload_system/logo.png">
+    </a>
+    <div>
+        <?= ($user['acc_type'] == 'administrator') ? '<a href="../admin_view/index.php" class="text-light" style="text-decoration: none; margin-right: 20px">Dashboard</a>' : '' ?>
+        
+        <!-- Dropdown for Profile -->
+        <div class="dropdown">
+            <button id="btnProfile" class="btn-profile dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?= ($user['emp_image'] != '') ? '<img src="../../upload_img/' . $user['emp_image'] . '" style="width: 40px; height: 40px; border-radius: 50%;">' :  '<i class="bi bi-person-fill"></i>' ?>
             </button>
-            
+            <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#">Settings</a>
+                <a class="dropdown-item" href="#">Logout</a>
+            </div>
         </div>
-    </nav>
+        
+    </div>
+</nav>
+
+
+
+
+
+    
+<!-- <li class="nav-item dropdown has-arrow main-drop">
+<a href="javascript:void(0);" class="dropdown-toggle nav-link userset" data-bs-toggle="dropdown">
+<span class="user-img"><img src="../../upload_img/<?= $db_emp_image?>" alt="">
+<span class="status online"></span></span>
+</a>
+<div class="dropdown-menu menu-drop-user">
+<div class="profilename" >
+<div class="profileset" onclick="window.location.href='profile.php?account_id=<?=$db_acc_id?>';">
+<span class="user-img"><img src="../../upload_img/<?= $db_emp_image?>" alt="">
+<span class="status online"></span></span>
+<div class="profilesets">
+<h6><?= $fullname?></h6>
+<h5><?= $db_acc_type?></h5>
+</div>
+
+</div>
+<hr class="m-0">
+<a class="dropdown-item" onclick="window.location.href='generalsettings.php?account_id=<?=$db_acc_id?>';"><i class="me-2" data-feather="settings"></i>Settings</a>
+<a class="dropdown-item" onclick="window.location.href='privacysettings.php?account_id=<?=$db_acc_id?>';"><i class="me-2" data-feather="lock"></i>Privacy</a>
+
+<hr class="m-0">
+<a class="dropdown-item logout pb-0" href="backend/logout.php"><img src="assets/img/icons/log-out.svg" class="me-2" alt="img">Logout</a>
+</div>
+</div>
+</li> -->
