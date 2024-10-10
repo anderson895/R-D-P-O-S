e27d386487a74e5c3772e7c3db5b5141686f4741
JARP <andersonandy046@gmail.com>
2024-10-10 23:42:30 +0800

    Update fetch_data.php

--- administrator/admin_view/productlist/controller/fetch_data.php
+++ administrator/admin_view/productlist/controller/fetch_data.php
@@ -6,6 +6,17 @@ include("../.../../../../../connection.php");
 
 $current_date = date("Y-m-d");
 
+// $query = "
+//     SELECT *,
+//         SUM(IF(s_expiration = '0000-00-00' OR s_expiration > '$current_date', s.s_amount, 0)) AS prod_stocks
+//     FROM product AS p
+//     LEFT JOIN stocks AS s ON p.prod_id = s.s_prod_id
+//     LEFT JOIN category AS c ON p.prod_category_id = c.category_id
+//     WHERE prod_status = '0' OR prod_status = '1'
+//     GROUP BY p.prod_id
+//     ORDER BY `prod_added` DESC
+// ";
+
 $query = "
     SELECT p.*, 
         SUM(IF(s.s_expiration = '0000-00-00' OR s.s_expiration > '$current_date', s.s_amount, 0)) AS prod_stocks
