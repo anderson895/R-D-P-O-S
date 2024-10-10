16d40a41fe3555056cf8d63ed664b4473d92b556
JARP <andersonandy046@gmail.com>
2024-10-10 23:50:27 +0800

    Update fetch_data.php

--- administrator/admin_view/productlist/controller/fetch_data.php
+++ administrator/admin_view/productlist/controller/fetch_data.php
@@ -6,16 +6,6 @@ include("../.../../../../../connection.php");
 
 $current_date = date("Y-m-d");
 
-// $query = "
-//     SELECT *,
-//         SUM(IF(s_expiration = '0000-00-00' OR s_expiration > '$current_date', s.s_amount, 0)) AS prod_stocks
-//     FROM product AS p
-//     LEFT JOIN stocks AS s ON p.prod_id = s.s_prod_id
-//     LEFT JOIN category AS c ON p.prod_category_id = c.category_id
-//     WHERE prod_status = '0' OR prod_status = '1'
-//     GROUP BY p.prod_id
-//     ORDER BY `prod_added` DESC
-// ";
 
 $query = "
     SELECT p.*, 
@@ -29,6 +19,18 @@ $query = "
     GROUP BY p.prod_id
     ORDER BY p.prod_added DESC
 ";
+
+// $query = "
+//     SELECT *,
+//         SUM(IF(s_expiration = '0000-00-00' OR s_expiration > '$current_date', s.s_amount, 0)) AS prod_stocks
+//     FROM product AS p
+//     LEFT JOIN stocks AS s ON p.prod_id = s.s_prod_id
+//     LEFT JOIN category AS c ON p.prod_category_id = c.category_id
+//     WHERE prod_status = '0' OR prod_status = '1'
+//     GROUP BY p.prod_id
+//     ORDER BY `prod_added` DESC
+// ";
+
 
 
 $result = mysqli_query($connections, $query);
