2194ece90a6d8053706f5ceadf4845b6f42790a8
JARP <andersonandy046@gmail.com>
2024-10-10 21:40:47 +0800

    Update pos.php

--- POS/pages/pos.php
+++ POS/pages/pos.php
@@ -1,7 +1,7 @@
 <?php
 include('../config/config.php');
 include('../functions/session.php');
-
+include("controller/maintinance.php");
 
 
 date_default_timezone_set('Asia/Manila');
@@ -108,7 +108,7 @@ if (isset($_POST['prod_id'], $_POST['amount'], $_POST['acc_id'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>POS</title>
-    <link rel="icon" href="../assets/images/logo.png" type="image/x-icon">
+    <link rel="icon" href="../upload_system/<?= $db_system_logo?>" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/inventory.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
