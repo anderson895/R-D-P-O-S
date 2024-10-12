e966e965cc277ce0ad7e8c72f3307a629d463749
JARP <andersonandy046@gmail.com>
2024-10-12 23:13:47 +0800

    Update order_notification.js

--- administrator/admin_view/Section/js/order_notification.js
+++ administrator/admin_view/Section/js/order_notification.js
@@ -6,36 +6,18 @@ const getOrdersCount = () => {
       type: 'GET',
       dataType: 'json',
       success: function(response) {
-          // The response will be in JSON format
-        console.log(response); // You can inspect the response in your browser console
+        console.log(response);
 
-          // Example of how you can handle the response:
           let pendingCount = response.Pending;
-        //   let acceptedCount = response.Accepted;
-        //   let readyForDeliveryCount = response.ReadyForDelivery;
-        //   let shippedCount = response.Shipped;
-        //   let deliveredCount = response.Delivered;
-        //   let collectedCount = response.collectedCount;
-        //   let rejected = response.Rejected;
-          
-        //   let cancelled = response.Cancelled;
-          // You can display these counts in your HTML or process them further
+       
           $('#pendingCount').text(pendingCount);
-        //   $('#acceptedCount').text(acceptedCount);
-        //   $('#readyForDeliveryCount').text(readyForDeliveryCount);
-        //   $('#shippedCount').text(shippedCount);
-        //   $('#deliveredCount').text(deliveredCount);
-        //   $('#collectedCount').text(collectedCount);
-        //   $('#rejectedCount').text(rejected);
-          
-        //     $('#cancelledCount').text(cancelled);
-          
 
-          
-          
-          
-        //   rejectedCount
-          
+          if (pendingCount > 0) {
+            $('#pendingCount').show();
+        } else {
+            $('#pendingCount').hide();
+        }
+   
           
           
       },
