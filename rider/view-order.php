4d6f47d45b606ac0569192a273db8109a2ca55b4
JARP <andersonandy046@gmail.com>
2024-10-12 23:46:55 +0800

    Update view-order.php

--- rider/view-order.php
+++ rider/view-order.php
@@ -145,30 +145,36 @@ if($getAddress->num_rows > 0) {
                         </h5>
                     </td>
                 </tr>
-                <tr>
-                    <td class="justify-content-between"><span style="font-weight: 600;">Order Date:</span></td>
-                    <td></td>
-                    <td></td>
-                    <td class="justify-content-between"><span style="font-weight: 600;"><?= (new DateTime($order['order_date']))->format('F j, Y, g:i a') ?></span></td>
-                </tr>
-                <tr>
-                    <td class="justify-content-between"><span style="font-weight: 600;">Address:</span></td>
-                    <td></td>
-                    <td></td>
-                    <td class="justify-content-between"><span style="font-weight: 600;"><?= $delAddress ?></span></td>
-                </tr>
-                <tr>
-                    <td class="justify-content-between"><span style="font-weight: 600;">Order By:</span></td>
-                    <td></td>
-                    <td></td>
-                    <td class="justify-content-between"><span style="font-weight: 600;"><?= $orderBy ?></span></td>
-                </tr>
-                <tr>
-                    <td class="justify-content-between"><span style="font-weight: 600;">Contac no.:</span></td>
-                    <td></td>
-                    <td></td>
-                    <td class="justify-content-between"><span style="font-weight: 600;"><?= $contactNumber ?></span></td>
-                </tr>
+
+
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td class="justify-content-between"><span style="font-weight: 600;">Order Date:</span></td>
+                            <td></td>
+                            <td></td>
+                            <td class="justify-content-between"><span style="font-weight: 600;"><?= (new DateTime($order['order_date']))->format('F j, Y, g:i a') ?></span></td>
+                        </tr>
+                        <tr>
+                            <td class="justify-content-between"><span style="font-weight: 600;">Address:</span></td>
+                            <td></td>
+                            <td></td>
+                            <td class="justify-content-between"><span style="font-weight: 600;"><?= $delAddress ?></span></td>
+                        </tr>
+                        <tr>
+                            <td class="justify-content-between"><span style="font-weight: 600;">Order By:</span></td>
+                            <td></td>
+                            <td></td>
+                            <td class="justify-content-between"><span style="font-weight: 600;"><?= $orderBy ?></span></td>
+                        </tr>
+                        <tr>
+                            <td class="justify-content-between"><span style="font-weight: 600;">Contac no.:</span></td>
+                            <td></td>
+                            <td></td>
+                            <td class="justify-content-between"><span style="font-weight: 600;"><?= $contactNumber ?></span></td>
+                        </tr>
+                    </tbody>
+                </table>
                 <?php
                 if ($order['status'] == 'Delivered') {
                 ?>
