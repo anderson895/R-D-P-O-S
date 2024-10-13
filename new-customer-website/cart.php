505251737389449434f564b1ce374342cb5f6d93
JARP <andersonandy046@gmail.com>
2024-10-14 00:43:05 +0800

    Update cart.php

--- new-customer-website/cart.php
+++ new-customer-website/cart.php
@@ -24,20 +24,19 @@ if ($getAddress->num_rows > 0) {
 <link rel="stylesheet" href="css/gallery.css">
 
 
-<div class="d-flex justify-content-between">
-    <h2><i class="bi bi-cart-check"></i> Cart</h2>
+<div class="d-flex justify-content-between align-items-center">
+    <h2 class="mb-0"><i class="bi bi-cart-check"></i> Cart</h2>
 
-    <!-- start check all -->
-    <div class="container mt-3 mb-3">
-            <div class="form-check mb-3">
-                <input type="checkbox" class="form-check-input" id="checkAll" style="width: 20px; height: 20px;">
-                <label class="form-check-label" for="checkAll"> Check All</label>
-            </div>
-        </div>
-    <!-- end check all -->
+    <!-- Start check all -->
+    <div class="form-check ms-3">
+        <input type="checkbox" class="form-check-input" id="checkAll" style="width: 20px; height: 20px;">
+        <label class="form-check-label" for="checkAll">Check All</label>
+    </div>
+    <!-- End check all -->
 
-    <?= ($getCartItems->num_rows > 0) ? '<button class="btn btn-danger" id="deleteAllItemsInCart"><i class="bi bi-trash3-fill"></i> Delete All</button>' : '' ?>
+    <?= ($getCartItems->num_rows > 0) ? '<button class="btn btn-danger ms-3" id="deleteAllItemsInCart"><i class="bi bi-trash3-fill"></i> Delete All</button>' : '' ?>
 </div>
+
 
 
 
