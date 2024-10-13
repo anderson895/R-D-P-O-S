a5d23904366d267a5c22b22085408b1bb94ab2ae
JARP <andersonandy046@gmail.com>
2024-10-13 21:40:48 +0800

    zoom

--- new-customer-website/Product_view.php
+++ new-customer-website/Product_view.php
@@ -41,11 +41,11 @@ $hidden_photos = ($photos == null) ? "hidden" : "";
                 <h5 class="modal-title" id="imageModalLabel">Image Preview</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="img-zoom-container">
+            <div class="modal-body img-zoom-container">
+               
                     <img id="myimage" src="../upload_prodImg/<?=$image?>" width="300" height="240" />
                     <div id="myresult" class="img-zoom-result"></div> <!-- Result placed inside the modal -->
-                </div>
+              
                 <!-- Optionally, you can include instructions or other content here -->
             </div>
         </div>
