1a417c1a8a1a896e64ed9fcf03ceeede79048fc9
JARP <andersonandy046@gmail.com>
2024-10-14 00:42:16 +0800

    Update cart.php

--- new-customer-website/cart.php
+++ new-customer-website/cart.php
@@ -26,19 +26,22 @@ if ($getAddress->num_rows > 0) {
 
 <div class="d-flex justify-content-between">
     <h2><i class="bi bi-cart-check"></i> Cart</h2>
+
+    <!-- start check all -->
+    <div class="container mt-3 mb-3">
+            <div class="form-check mb-3">
+                <input type="checkbox" class="form-check-input" id="checkAll" style="width: 20px; height: 20px;">
+                <label class="form-check-label" for="checkAll"> Check All</label>
+            </div>
+        </div>
+    <!-- end check all -->
+
     <?= ($getCartItems->num_rows > 0) ? '<button class="btn btn-danger" id="deleteAllItemsInCart"><i class="bi bi-trash3-fill"></i> Delete All</button>' : '' ?>
 </div>
 
 
-<!-- start check all -->
-    <div class="container mt-3">
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="checkAll" style="width: 20px; height: 20px;">
-            <label class="form-check-label" for="checkAll"> Check All</label>
-        </div>
-    </div>
-<!-- end check all -->
-<div class="container card cart-items-container " id="cartItemsContainer">
+
+<div class="container card cart-items-container mt-5" id="cartItemsContainer">
 
 </div>
 <?php
