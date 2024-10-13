769ceecd3733a674628f012714dcb933ab58ed01
JARP <andersonandy046@gmail.com>
2024-10-13 21:20:57 +0800

    Update Product_view.php

--- new-customer-website/Product_view.php
+++ new-customer-website/Product_view.php
@@ -33,28 +33,25 @@ $hidden_photos = ($photos == null) ? "hidden" : "";
         <div class="row mt-4">
             <div class="text-center">
                 <img id="mainImage" src="../upload_prodImg/<?=$image?>" alt="Product Image" class="product-image mb-4" data-bs-toggle="modal" data-bs-target="#imageModal">
-             <!-- Modal Structure -->
-<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true" data-bs-backdrop="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg"> <!-- Use modal-lg for larger images -->
+<!-- Modal Structure -->
+<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true" data-bs-backdrop="static">
+    <div class="modal-dialog modal-dialog-centered modal-lg"> <!-- Use modal-lg for a larger modal -->
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="imageModalLabel">Image Preview</h5> <!-- Modal Title -->
+                <h5 class="modal-title" id="imageModalLabel">Image Preview</h5> <!-- Title for accessibility -->
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="img-zoom-container text-center"> <!-- Center the image -->
-                    <img id="myimage" src="../upload_prodImg/<?=$image?>" class="img-fluid" alt="Preview Image" /> <!-- Responsive image -->
+            <div class="modal-body text-center"> <!-- Center the content -->
+                <div class="img-zoom-container">
+                    <img id="myimage" src="../upload_prodImg/<?=$image?>" class="img-fluid" alt="Image Preview" /> <!-- Use img-fluid for responsiveness -->
                     <div id="myresult" class="img-zoom-result"></div>
                 </div>
-                <p class="text-center mt-2">Hover to zoom in on the image.</p> <!-- Instructions -->
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <!-- Navigation buttons can be added here if needed -->
+                <p class="mt-3">Hover to zoom in on the image.</p> <!-- Optional instructions -->
             </div>
         </div>
     </div>
 </div>
+
 
 
 
