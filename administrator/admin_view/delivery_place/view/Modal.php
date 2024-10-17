a8a1732c4ce9717b3cdf7bfe5313316c48521fe9
JARP <andersonandy046@gmail.com>
2024-10-17 23:59:43 +0800

    Update Modal.php

--- administrator/admin_view/delivery_place/view/Modal.php
+++ administrator/admin_view/delivery_place/view/Modal.php
@@ -78,7 +78,10 @@
         <div class="form-group m-0 bg-light rounded">
                         <textarea style="display:none;" disabled id="complete_address_add" cols="30" rows="10"></textarea>
                         <div class="search-container">
-                            <input required type="text" class="form-control" id="searchBarangay_add" placeholder="Search brgy.." name="searchBarangay_add">
+                            <div class="form-floating mb-3">
+                              <input required type="text" class="form-control" id="searchBarangay_add" placeholder="Search brgy.." name="searchBarangay_add">
+                              <label for="Addshipping">Search barangay</label>
+                            </div>
                         </div>
                         <div id="barangaySuggestions_add" class="suggestions-row ml-4"></div>
                         <input hidden type="text" id="region_add">
