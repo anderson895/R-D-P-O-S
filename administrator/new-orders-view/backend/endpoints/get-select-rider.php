eda70264952356993eaac0bdb38a7c89c85516ed
JARP <andersonandy046@gmail.com>
2024-10-18 03:46:27 +0800

    Update get-select-rider.php

--- administrator/new-orders-view/backend/endpoints/get-select-rider.php
+++ administrator/new-orders-view/backend/endpoints/get-select-rider.php
@@ -59,7 +59,9 @@ if (isset($_GET['orderId'])) {
 ?>
             <select class="form-control" id="selectRider" data-id="<?= $order['order_id'] ?>">
                 <option selected disabled>Select Rider</option>
-                <option value="<?= $user['acc_id'] ?>" <?= ($user['acc_id'] == $order['rider_id']) ? 'selected' : '' ?>><?= $user['acc_fname'] . ' ' . $user['acc_lname'] ?></option>
+                <option value="<?= $user['acc_id'] ?>" <?= ($user['acc_id'] == $order['rider_id']) ? 'selected' : '' ?>>
+                    <?= $user['acc_fname'] . ' ' . $user['acc_lname'] ?>(0)
+                </option>
                 <?php
                 $getRiders = $db->getDelivery();
                 while ($rider = $getRiders->fetch_assoc()) {
