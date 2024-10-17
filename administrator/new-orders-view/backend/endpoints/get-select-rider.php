3615cdecc05f3237effefb773a2ac282d4b1c5d8
JARP <andersonandy046@gmail.com>
2024-10-18 01:59:26 +0800

    Update get-select-rider.php

--- administrator/new-orders-view/backend/endpoints/get-select-rider.php
+++ administrator/new-orders-view/backend/endpoints/get-select-rider.php
@@ -57,7 +57,7 @@ if (isset($_GET['orderId'])) {
 
         if ($order['status'] == 'Pending' || $order['status'] == 'Accepted' || $order['status'] == 'Ready For Delivery') {
 ?>
-            <select hidden class="form-control" id="selectRider" data-id="<?= $order['order_id'] ?>">
+            <select class="form-control" id="selectRider" data-id="<?= $order['order_id'] ?>">
                 <option selected disabled>Select Rider</option>
                 <option value="<?= $user['acc_id'] ?>" <?= ($user['acc_id'] == $order['rider_id']) ? 'selected' : '' ?>><?= $user['acc_fname'] . ' ' . $user['acc_lname'] ?></option>
                 <?php
@@ -69,7 +69,6 @@ if (isset($_GET['orderId'])) {
                 }
                 ?>
             </select>
-            
 <?php
         } else {
             $riderName = 'N/A';
