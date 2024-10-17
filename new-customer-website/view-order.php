8ef98cd01471ef30109c4bb4874c049d1f45d52b
JARP <andersonandy046@gmail.com>
2024-10-17 23:21:47 +0800

    Update view-order.php

--- new-customer-website/view-order.php
+++ new-customer-website/view-order.php
@@ -60,7 +60,9 @@ if ($getAddress->num_rows > 0) {
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css" />
 <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 
-<style>
+<!-- <style>
+    
+
 table {
             width: 100%; 
         }
@@ -78,7 +80,7 @@ table {
         }
 
 
-</style>
+</style> -->
 
 <div class="d-flex justify-content-between">
     <a href="orders.php?page=<?= $order['status'] ?>" class="btn" style="font-size: 20px;"><i
@@ -115,144 +117,181 @@ table {
 
 <div class="table-responsive">
     <table class="table">
-        <thead>
+            <thead>
             <tr>
                 <td colspan="5">
-                    <h5 class="font-weight-bold mt-3 mb-2 text-secondary">
+                    <h5 style="font-weight: 700;" class="mt-3 mb-2 text-secondary">
                         <i class="bi bi-feather"></i> Items
                     </h5>
                 </td>
             </tr>
             <tr>
-                <th>#</th>
-                <th>Item</th>
-                <th>Quantity</th>
-                <th>Amount</th>
+                <th class="">#</th>
+                <th class="">Item</th>
+                <th class="">Quantity</th>
+                <th class="">Amount</th>
                 <?php if ($order['status'] == 'Delivered') { ?>
-                    <th>Actions</th>
+                    <th class="">Actions</th>
                 <?php } ?>
             </tr>
         </thead>
+
         <tbody>
             <?php
             $count = 1;
             while ($orderItem = $getOrderItems->fetch_assoc()) {
-            ?>
+                ?>
                 <tr style="font-size: 0.9rem;">
                     <td class="pt-3"><?= $count ?></td>
                     <td class="">
-                        <img src="../upload_prodImg/<?= $orderItem['prod_image'] ?>" alt="<?= $orderItem['prod_name'] ?>" style="height: 40px; width: 40px;" class="me-2">
+                        <img src="../upload_prodImg/<?= $orderItem['prod_image'] ?>" style="height: 40px; width: 40px;">
                         <?= $orderItem['prod_name'] ?>
                     </td>
                     <td class="pt-3"><?= $orderItem['qty'] . ' x ' . $orderItem['prod_currprice'] ?></td>
-                    <td class="pt-3">₱<?= number_format($Amount = $orderItem['qty'] * $orderItem['prod_currprice'], 2) ?></td>
-                    <?php if ($order['status'] == 'Delivered') { ?>
+                    <td class="pt-3">₱<?= number_format($Amount=$orderItem['qty'] * $orderItem['prod_currprice'],2) ?></td>
+
+
+                    <?php if ($order['status'] == 'Delivered') { 
+                        echo '
                         <td class="pt-3">
-                            <button class="btn btn-success rateToggler" data-bs-toggle="modal" data-bs-target="#rateTsModal" data-prod_id="<?= $orderItem['prod_id'] ?>" data-prod_name="<?= $orderItem['prod_name'] ?>">Feedback</button>
-                        </td>
-                    <?php } ?>
+                            <button class="btn btn-success rateToggler" data-bs-toggle="modal" data-bs-target="#rateTsModal"
+                            data-prod_id="'.$orderItem['prod_id'].'"
+                            data-prod_name="'.$orderItem['prod_name'].'">Feedback</button>
+                        </td>';
+                    } ?>
+
+
                 </tr>
-            <?php
+                <?php
                 $count++;
             }
             ?>
             <tr>
-                <td colspan="5">
-                    <h5 class="font-weight-bold mt-5 mb-2 text-secondary">
+                <td colspan="4">
+                    <h5 style="font-weight: 700;" class="mt-5 mb-2 text-secondary">
                         <i class="bi bi-currency-exchange"></i> Order Details
                     </h5>
                 </td>
             </tr>
             <tr>
-                <td><span class="font-weight-bold">Subtotal:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold">₱<?= number_format($order['subtotal'], 2) ?></span></td>
+                <td class="justify-content-between"><span class="" style="font-weight: 600;">Subtotal:</span></td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span class=""
+                        style="font-weight: 600;">₱<?= number_format($order['subtotal'],2) ?></span></td>
             </tr>
             <tr>
-                <td><span class="font-weight-bold">VAT:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold">₱<?= number_format($order['vat'], 2) ?></span></td>
+                <td class="justify-content-between"><span class="" style="font-weight: 600;">Vat:</span></td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span class="" style="font-weight: 600;">₱<?= number_format($order['vat'],2) ?></span>
+                </td>
             </tr>
             <tr>
-                <td><span class="font-weight-bold">Shipping Fee:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold">₱<?= number_format($order['sf'], 2) ?></span></td>
+                <td class="justify-content-between"><span class="" style="font-weight: 600;">Shipping Fee:</span></td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span class="" style="font-weight: 600;">₱<?= number_format($order['sf'],2) ?></span>
+                </td>
             </tr>
             <tr>
-                <td><span class="text-success font-weight-bold">Total:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="text-success font-weight-bold">₱<?= number_format($order['total'], 2) ?></span></td>
+                <td class="justify-content-between"><span class="text-success" style="font-weight: 700;">Total:</span>
+                </td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span class="text-success"
+                        style="font-weight: 700;">₱<?= number_format($order['total'],2) ?></span></td>
             </tr>
             <tr>
-                <td colspan="5">
-                    <h5 class="font-weight-bold mt-5 mb-2 text-secondary">
+                <td colspan="4">
+                    <h5 style="font-weight: 700;" class="mt-5 mb-2 text-secondary">
                         <i class="bi bi-truck"></i> Delivery Details
                     </h5>
                 </td>
             </tr>
-        </tbody>
-    </table>
-    
-    <table class="table">
-        <tbody>
-            <tr>
-                <td><span class="font-weight-bold">Address:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold"><?= $userAddress ?></span></td>
-            </tr>
-            <tr>
-                <td><span class="font-weight-bold">Order Date:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold"><?= (new DateTime($order['order_date']))->format('F j, Y, g:i a') ?></span></td>
-            </tr>
-            <?php if ($order['status'] == 'Delivered') { ?>
+
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td class="justify-content-between"><span style="font-weight: 600;">Address:</span></td>
+                        <td></td>
+                        <td></td>
+                        <td class="justify-content-between"><span style="font-weight: 600;"><?= $userAddress ?></span></td>
+                    </tr>
+                    <tr>
+                        <td class="justify-content-between"><span style="font-weight: 600;">Order Date:</span></td>
+                        <td></td>
+                        <td></td>
+                        <td class="justify-content-between"><span
+                                style="font-weight: 600;"><?= (new DateTime($order['order_date']))->format('F j, Y, g:i a') ?></span>
+                        </td>
+                    </tr>
+                
+            <?php
+            if ($order['status'] == 'Delivered') {
+                ?>
                 <tr>
-                    <td><span class="font-weight-bold">Delivered Date:</span></td>
-                    <td colspan="2"></td>
-                    <td class="text-end"><span class="font-weight-bold"><?= (new DateTime($order['delivered_date']))->format('F j, Y, g:i a') ?></span></td>
+                    <td class="justify-content-between"><span style="font-weight: 600;">Delivered Date:</span></td>
+                    <td></td>
+                    <td></td>
+                    <td class="justify-content-between"><span
+                            style="font-weight: 600;"><?= (new DateTime($order['delivered_date']))->format('F j, Y, g:i a') ?></span>
+                    </td>
                 </tr>
-            <?php } ?>
+                <?php
+            }
+            ?>
             <tr>
-                <td><span class="font-weight-bold">Delivery Rider:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold"><?= $rider ?></span></td>
+                <td class="justify-content-between"><span style="font-weight: 600;">Delivery Rider:</span></td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span style="font-weight: 600;"><?= $rider ?></span></td>
             </tr>
             <tr>
-                <td><span class="font-weight-bold">Delivery Contact No:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold"><?= $riderContactNo ?></span></td>
+                <td class="justify-content-between"><span style="font-weight: 600;">Delivery Contact No:</span></td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span style="font-weight: 600;"><?= $riderContactNo ?></span></td>
             </tr>
-        </tbody>
-    </table>
-    
-    <table class="table">
-        <tbody>
+
+                </body>
+            </table>
             <tr>
                 <td colspan="4">
-                    <h5 class="font-weight-bold mt-5 mb-2 text-secondary">
+                    <h5 style="font-weight: 700;" class="mt-5 mb-2 text-secondary">
                         <i class="bi bi-credit-card-2-back"></i> Payment Details
                     </h5>
                 </td>
             </tr>
             <tr>
-                <td><span class="font-weight-bold">Payment Type:</span></td>
-                <td colspan="2"></td>
-                <td class="text-end"><span class="font-weight-bold"><?= $paymentType ?></span></td>
+                <td class="justify-content-between"><span style="font-weight: 600;">Payment Type:</span></td>
+                <td></td>
+                <td></td>
+                <td class="justify-content-between"><span style="font-weight: 600;"><?= $paymentType ?></span></td>
             </tr>
-            <?php if ($paymentType != 'COD') { ?>
+            <?php
+            if ($paymentType != 'COD') {
+                ?>
                 <tr>
                     <td colspan="4" class="text-center">
-                        <h5 class="text-success mt-5"><i>Proof of Payment</i></h5>
-                        <div class="pof-container">
-                            <img src="backend/proof-of-payment/<?= $order['pof'] ?>" alt="Proof of Payment" class="img-fluid" style="max-width: 300px;">
+                        <center class="mt-5">
+                            <h5 class="text-success">
+                                <i>
+                                    Proof of Payment
+                                </i>
+                            </h5>
+                        </center>
+                        <div class="pof-container container">
+                            <img src="backend/proof-of-payment/<?= $order['pof'] ?>" style="max-width: 300px;">
                         </div>
                     </td>
                 </tr>
-            <?php } ?>
+                <?php
+            }
+            ?>
         </tbody>
     </table>
 </div>
-
 
 </div>
 
