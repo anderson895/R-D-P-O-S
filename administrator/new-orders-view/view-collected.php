674b41ee113d5e434b644e907f6db966982faabb
JARP <andersonandy046@gmail.com>
2024-10-22 21:38:20 +0800

    Update view-collected.php

--- administrator/new-orders-view/view-collected.php
+++ administrator/new-orders-view/view-collected.php
@@ -72,7 +72,7 @@ $page = $_GET['rider_id'];
                                         <a href="view-order.php?orderId==<?=$order['order_id'];?>"><?=$order['order_id'];?></a>
                                     </td>
                                     <td>
-                                        <a href="../admin_view/profile_customer.php?target_id=<?=$order['acc_id'];?>"><?=ucfirst($order['customer_name']);?></a>
+                                        <a href="../../admin_view/profile_customer.php?target_id=<?=$order['acc_id'];?>"><?=ucfirst($order['customer_name']);?></a>
                                     </td>
 
                                     <td>₱ <?= $order['total_sales'] ?></td>
