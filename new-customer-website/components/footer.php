85dd4b21829faf29de747a4d2dfef9d2a5cf6609
JARP <andersonandy046@gmail.com>
2024-10-22 14:39:43 +0800

    awdaw

--- new-customer-website/components/footer.php
+++ new-customer-website/components/footer.php
@@ -514,7 +514,25 @@
 <!-- End of user Password Modal -->
 
 
-
+<!-- Start user Modal -->
+<div class="modal fade" id="cancelModal" tabindex="-1" role="dialog" aria-labelledby="cancelModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="cancelModalLabel">Cancel Order</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are you sure you want to cancel this order?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-danger">Confirm Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End user Modal -->
 
 
 
