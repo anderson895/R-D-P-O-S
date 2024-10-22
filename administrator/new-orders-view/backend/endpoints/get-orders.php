fd912e6f073445f6bc68be8206a7505fecec6a41
JARP <andersonandy046@gmail.com>
2024-10-22 15:14:43 +0800

    Update get-orders.php

--- administrator/new-orders-view/backend/endpoints/get-orders.php
+++ administrator/new-orders-view/backend/endpoints/get-orders.php
@@ -13,7 +13,8 @@ if (isset($_GET['page'])) {
                 $rider =  $getRider->fetch_assoc();
                 $riderName = $rider['acc_fname'] . ' ' . $rider['acc_lname'];
             }
-            $reason=$order['reject_reason'];
+            $reject_reason=$order['reject_reason'];
+            $cancel_reason=$order['cancel_reason'];
             
 ?>
             <tr class="orders-tr">
@@ -27,8 +28,8 @@ if (isset($_GET['page'])) {
                 <td><?= date('F j, Y g:i A', strtotime($order['order_date'])) ?></td>
                 <?= ($page == 'Delivered') ? '<td>' . date('F j, Y g:i A', strtotime($order['delivered_date'])) . '</td>' : '' ?>
                 <?= ($page == 'Delivered' || $page == 'Shipped') ? '<td>' . ucfirst($riderName) . '</td>' : '' ?>
-                <?= ($page == 'Rejected') ? '<td>' . ucfirst($reason) . '</td>' : '' ?>
-                <?= ($page == 'Cancelled') ? '<td>' . ucfirst($reason) . '</td>' : '' ?>
+                <?= ($page == 'Rejected') ? '<td>' . ucfirst($reasreject_reasonon) . '</td>' : '' ?>
+                <?= ($page == 'Cancelled') ? '<td>' . ucfirst($cancel_reason) . '</td>' : '' ?>
     
             </tr>
         <?php
