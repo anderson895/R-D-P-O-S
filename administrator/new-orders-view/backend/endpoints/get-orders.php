c9e4486ceed6ab52a457a215d15282bae7ae6d33
JARP <andersonandy046@gmail.com>
2024-10-24 15:34:46 +0800

    Update get-orders.php

--- administrator/new-orders-view/backend/endpoints/get-orders.php
+++ administrator/new-orders-view/backend/endpoints/get-orders.php
@@ -27,7 +27,7 @@ if (isset($_GET['page'])) {
                 <td>
                     <?php 
                     if($order['unsucessful_reason']){
-                        echo "<b class='text-danger btnShowReason' data-reason='" . $order['unsucessful_reason'] . "' data-delivery_date='" . date('F j, Y g:i A', strtotime($order['delivered_date'])) . "'>Re-Deliver</b>";
+                        echo "<b class='text-danger btnShowReason cursor-pointer' data-reason='" . $order['unsucessful_reason'] . "' data-delivery_date='" . date('F j, Y g:i A', strtotime($order['delivered_date'])) . "'>Re-Deliver</b>";
                     } else {
                         echo "<b class='text-success'>New Order</b>"; 
                     }
