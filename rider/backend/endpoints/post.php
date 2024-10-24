013ca6f9890e3e4f247491f2a646e9e3ffcaa3ae
JARP <andersonandy046@gmail.com>
2024-10-24 14:34:07 +0800

    frmUnsuccessfulOrderStatus

--- rider/backend/endpoints/post.php
+++ rider/backend/endpoints/post.php
@@ -2,10 +2,6 @@
 include('../class.php');
 $db = new global_class();
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-
 if (isset($_POST['requestType'])) {
     if ($_POST['requestType'] == 'UpgradeOrderStatus') {
         echo $db->productDelivered($_POST['orderId'], $_FILES['proofOfDel']);
