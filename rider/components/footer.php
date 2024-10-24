c0ca978949e4af17c37c0065e8b6c145502ea098
JARP <andersonandy046@gmail.com>
2024-10-24 15:02:19 +0800

    Update footer.php

--- rider/components/footer.php
+++ rider/components/footer.php
@@ -46,10 +46,10 @@
                         <label>Unsuccessful Reason</label>
                         <select class="form-select" name="unsuccessfulReason" id="unsuccessfulReason">
                             <option value="">-- Select a reason --</option>
-                            <option value="OutOfStock">Out of Stock</option>
-                            <option value="PaymentFailed">Payment Failed</option>
-                            <option value="CustomerCancelled">Customer Unavailable</option>
-                            <option value="DeliveryIssues">Delivery Issues</option>
+                            <option value="Out of Stock">Out of Stock</option>
+                            <option value="Payment Failed">Payment Failed</option>
+                            <option value="Customer Unavailable">Customer Unavailable</option>
+                            <option value="Delivery Issues">Delivery Issues</option>
                         </select>
                     </div>
                 </div>
