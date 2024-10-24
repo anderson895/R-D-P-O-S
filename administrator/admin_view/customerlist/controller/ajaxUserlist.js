367da848b86e6237f029c70b2139b9a670dad999
JARP <andersonandy046@gmail.com>
2024-10-24 18:43:02 +0800

    Update ajaxUserlist.js

--- administrator/admin_view/customerlist/controller/ajaxUserlist.js
+++ administrator/admin_view/customerlist/controller/ajaxUserlist.js
@@ -68,72 +68,65 @@ $(document).ready(function () {
 
     // Listen for the checkbox change event
     // $(".check").on("change", function () {
-      $(document).on("change", ".check", function() {
-      var checkbox = $(this);
-      var session_id = $('#session_id').val();
 
-      if (checkbox.is(":checked")) {
-        // Checkbox is checked, show "Enable" message
-        var message = "Are you sure you want to enable this account?";
-      } else {
-        // Checkbox is unchecked, show "Disable" message
-        var message = "Are you sure you want to disable this account?";
-      }
+          $(document).on("change", ".check", function() {
+          var checkbox = $(this);
+          var session_id = $('#session_id').val();
+          var acc_id = checkbox.val(); // Traverse the DOM to find the associated acc_id
 
-      // Traverse the DOM to find the associated acc_id
-      var acc_id = checkbox.val();
+          // Determine the message based on the checkbox state
+          var message = checkbox.is(":checked") 
+              ? "Are you sure you want to enable this account?" 
+              : "Are you sure you want to disable this account?";
 
-      // Show the initial SweetAlert with "Yes" and "Cancel" options
-      Swal.fire({
-        title: "Are you sure?",
-        text: message,
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes",
-        cancelButtonText: "Cancel",
-        confirmButtonClass: "btn btn-primary",
-        cancelButtonClass: "btn btn-danger ml-1",
-        buttonsStyling: false,
-      }).then(function (result) {
-        if (result.value) {
-          // If the user clicks "Yes," proceed with the AJAX request
-          $.ajax({
-            url: 'userlist/controller/changeUserStatus_user.php',
-            type: 'POST',
-            data: {
-              acc_id: acc_id,
-              session_id: session_id,
-              acc_status: checkbox.is(":checked") ? 0 : 2
-            },
-            success: function (response) {
-              // Handle the success response
-              Swal.fire({
-                type: "success",
-                title: "Action Successful",
-                text: "Account has been " + (checkbox.is(":checked") ? "enabled" : "disabled") + ".",
-                confirmButtonClass: "btn btn-success"
-              });
-
-              // Log the response inside the success callback
-             // console.log(response);
-             
-            },
-            error: function (xhr, status, error) {
-              // Handle the error response
-              Swal.fire({
-                type: "error",
-                title: "Error",
-                text: "An error occurred while enabling/disabling the account.",
-                confirmButtonClass: "btn btn-danger"
-              });
-            }
+          // Show the initial SweetAlert with "Yes" and "Cancel" options
+          Swal.fire({
+              title: "Are you sure?",
+              text: message,
+              icon: "warning", // Changed from 'type' to 'icon' for SweetAlert2
+              showCancelButton: true,
+              confirmButtonColor: "#3085d6",
+              cancelButtonColor: "#d33",
+              confirmButtonText: "Yes",
+              cancelButtonText: "Cancel",
+              confirmButtonClass: "btn btn-primary",
+              cancelButtonClass: "btn btn-danger ml-1",
+              buttonsStyling: false,
+          }).then(function(result) {
+              if (result.isConfirmed) { // Changed from result.value to result.isConfirmed
+                  // If the user clicks "Yes," proceed with the AJAX request
+                  $.ajax({
+                      url: 'userlist/controller/changeUserStatus_user.php',
+                      type: 'POST',
+                      data: {
+                          acc_id: acc_id,
+                          session_id: session_id,
+                          acc_status: checkbox.is(":checked") ? 0 : 2
+                      },
+                      success: function(response) {
+                          // Handle the success response
+                          Swal.fire({
+                              icon: "success", // Changed from 'type' to 'icon'
+                              title: "Action Successful",
+                              text: "Account has been " + (checkbox.is(":checked") ? "enabled" : "disabled") + ".",
+                              confirmButtonClass: "btn btn-success"
+                          });
+                      },
+                      error: function(xhr, status, error) {
+                          // Handle the error response
+                          Swal.fire({
+                              icon: "error", // Changed from 'type' to 'icon'
+                              title: "Error",
+                              text: "An error occurred while enabling/disabling the account.",
+                              confirmButtonClass: "btn btn-danger"
+                          });
+                      }
+                  });
+              } else {
+                  // If the user clicks "Cancel," revert the checkbox state
+                  checkbox.prop('checked', !checkbox.prop('checked'));
+              }
           });
-        } else {
-          // If the user clicks "Cancel," handle any necessary actions (e.g., toggle the checkbox state)
-          checkbox.prop('checked', !checkbox.prop('checked'));
-        }
       });
-    });
+
   });
