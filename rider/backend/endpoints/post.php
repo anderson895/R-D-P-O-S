44ec1fc33c9e15a9b81c3225ace736ecdc31269f
JARP <andersonandy046@gmail.com>
2024-10-24 14:58:51 +0800

    Update post.php

--- rider/backend/endpoints/post.php
+++ rider/backend/endpoints/post.php
@@ -10,6 +10,6 @@ if (isset($_POST['requestType'])) {
     if ($_POST['requestType'] == 'UpgradeOrderStatus') {
         echo $db->productDelivered($_POST['orderId'], $_FILES['proofOfDel']);
     }else if ($_POST['requestType'] == 'UnsuccessOrderStatus') {
-        echo $db->productUnsucessful($_POST['orderId'], $_FILES['unsuccessfulReason']);
+        echo $db->productUnsucessful($_POST['orderId'], $_POST['unsuccessfulReason']);
     }
 }
