7453a182775967872de1c4738c384e2cd990f87e
JARP <andersonandy046@gmail.com>
2024-10-24 16:36:01 +0800

    Update table.php

--- administrator/admin_view/managefeedback/view/table.php
+++ administrator/admin_view/managefeedback/view/table.php
@@ -48,14 +48,14 @@
 
                             $r_feedback = $rate['r_feedback'];
 
-                            if($r_status = $rate['r_status']=='1'){
-                                $button_for_approval = '<button type="button" class="btn btn-secondary"><i class="fas fa-times"></i></button>';
-                                $feed_status='<b class="alert alert-success text-success">Allowed</b>';              
-                            }else if($r_status = $rate['r_status']=='2'){
-                                $button_for_approval='<button type="button" class="btn btn-success"><i class="fas fa-check"></i></button>';
-                                $feed_status='<b class="alert alert-secondary text-secondary">Waiting for approval</b>';
-                               
+                            if ($r_status = $rate['r_status'] == '1') {
+                                $button_for_approval = '<button type="button" class="btn btn-secondary btn-block"><i class="fas fa-times"></i></button>';
+                                $feed_status = '<b class="alert alert-success text-success">Allowed</b>';
+                            } else if ($r_status = $rate['r_status'] == '2') {
+                                $button_for_approval = '<button type="button" class="btn btn-success btn-block"><i class="fas fa-check"></i></button>';
+                                $feed_status = '<b class="alert alert-secondary text-secondary">Waiting for approval</b>';
                             }
+                            
                         ?>
                         <tr>
                             <td><?= $count ?></td>
