134b57cfa94bdcaabf7a964940779b7847b86c07
JARP <andersonandy046@gmail.com>
2024-10-24 17:35:55 +0800

    Update LenFunction.js

--- administrator/admin_view/managefeedback/js/LenFunction.js
+++ administrator/admin_view/managefeedback/js/LenFunction.js
@@ -11,6 +11,57 @@ $(document).ready(function() {
     
 
     
+      // Listen for the checkbox change event
+      $(document).on("click", ".btnRequestToRestrict", function (e) {
+        var r_id = $(this).attr('data-id');
+        
+        console.log(r_id);
+  
+  
+        // Show the initial SweetAlert with "Yes" and "Cancel" options
+        Swal.fire({
+          title: "Are you sure?",
+          text: "Are you sure you want to remove this feedback on the display?",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes",
+          cancelButtonText: "Cancel",
+          confirmButtonClass: "btn btn-primary",
+          cancelButtonClass: "btn btn-danger ml-1",
+          buttonsStyling: false,
+        }).then(function (result) {
+          if (result.value) {
+            // If the user clicks "Yes," proceed with the AJAX request
+            $.ajax({
+              url: 'managefeedback/controller/post.php',
+              type: 'POST',
+              data: {
+                r_id: r_id,
+                SubmitType:"RestrictRevs",
+              },
+              success: function (response) {
+
+                console.log(response);
+                // Handle the success response
+                Swal.fire({
+                  type: "success",
+                  title: "Action Successful",
+                  text: "Feedback remove on the display successful",
+                  confirmButtonClass: "btn btn-success"
+                });
+
+              },
+            });
+          } else {
+            
+          }
+        });
+      });
+
+
+    
     // Listen for the checkbox change event
     $(document).on("click", ".btnRequestToAllowed", function (e) {
         var r_id = $(this).attr('data-id');
@@ -64,54 +115,6 @@ $(document).ready(function() {
 
 
 
-      // Listen for the checkbox change event
-    $(document).on("click", ".btnRequestToRestrict", function (e) {
-        var r_id = $(this).attr('data-id');
-        
-        console.log(r_id);
-  
-  
-        // Show the initial SweetAlert with "Yes" and "Cancel" options
-        Swal.fire({
-          title: "Are you sure?",
-          text: "Are you sure you want to remove this feedback on the display?",
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes",
-          cancelButtonText: "Cancel",
-          confirmButtonClass: "btn btn-primary",
-          cancelButtonClass: "btn btn-danger ml-1",
-          buttonsStyling: false,
-        }).then(function (result) {
-          if (result.value) {
-            // If the user clicks "Yes," proceed with the AJAX request
-            $.ajax({
-              url: 'managefeedback/controller/post.php',
-              type: 'POST',
-              data: {
-                r_id: r_id,
-                SubmitType:"RestrictRevs",
-              },
-              success: function (response) {
-
-                console.log(response);
-                // Handle the success response
-                Swal.fire({
-                  type: "success",
-                  title: "Action Successful",
-                  text: "Feedback remove on the display successful",
-                  confirmButtonClass: "btn btn-success"
-                });
-
-              },
-            });
-          } else {
-            
-          }
-        });
-      });
 
 
 
