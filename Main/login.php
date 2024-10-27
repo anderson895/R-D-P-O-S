6b6b22e4b20487fad1cb35cfd85677de3050a828
JARP <andersonandy046@gmail.com>
2024-10-27 17:02:20 +0800

    Update login.php

--- Main/login.php
+++ Main/login.php
@@ -39,55 +39,58 @@ $current_date = date('Y-m-d');
 
        
 
-        <div class="main-wrapper" style="background-color:#F7F7F7; ">
-            <div class="account-content">
-                <div class="login-wrapper">
+      <div class="main-wrapper" style="background-color:#F7F7F7; ">
+        <div class="account-content">
+            <div class="login-wrapper">
                 <div class="login-content">
-                <div class="container bg-white rounded shadow p-4" style="max-width: 400px;">
-                    <div class="login-userset">
-                        <form method="POST">
-                            <div class="login-userheading text-center mb-4">
-                                <h3>Sign In</h3>
-                            </div>
+                    <div class="container" style="background-color:white; border-radius:15px;">
+                        <div class="login-userset">
+                            <form method="POST">
+                                <div class="login-userheading mt-3">
+                                    <h3>Sign In</h3>
+                                </div>
 
-                            <!-- Username Field -->
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="username" placeholder="Enter username" name="email_or_username" required>
-                                <label for="username">Username</label>
-                            </div>
+                                <!-- Username Field -->
+                                <div class="form-login">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" id="username" placeholder="Enter username" name="email_or_username" required>
+                                        <label for="username">Username</label>
+                                    </div>
+                                </div>
 
-                            <!-- Password Field -->
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password" placeholder="Enter your password" name="password" required>
-                                <label for="password">Password</label>
-                            </div>
+                                <!-- Password Field -->
+                                <div class="form-login">
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control" id="password" placeholder="Enter your password" name="password" required>
+                                        <label for="password">Password</label>
+                                    </div>
+                                </div>
 
-                            <div class="mb-3 text-end">
-                                <h4><a href="forgetpassword.php" class="text-decoration-none">Forgot Password?</a></h4>
-                            </div>
+                                <div class="form-login">
+                                    <div class="alreadyuser">
+                                        <h4><a href="forgetpassword.php" class="hover-a">Forgot Password?</a></h4>
+                                    </div>
+                                </div>
 
-                            <!-- <div class="mb-3">
-                                <button class="btn btn-primary w-100" type="submit" name="btnLogin">Sign In</button>
-                            </div> -->
-                            <div class="form-login">
-                                        <button class="btn btn-login" type="submit" name="btnLogin">Sign In</button>
-                            </div>
-                        </form>
+                                <div class="form-login">
+                                    <button class="btn btn-login" type="submit" name="btnLogin">Sign In</button>
+                                </div>
+                            </form>
 
-                        <div class="signinform text-center">
-                            <h4>Don’t have an account? <a href="register.php" class="text-decoration-none">Sign Up</a></h4>
+                            <div class="signinform text-center">
+                                <h4>Don’t have an account? <a href="register.php" class="hover-a">Sign Up</a></h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-                    <div class="login-img">
-                        <div class="container">
-                            <img src="../upload_system/<?=$db_system_banner?>" alt="">
-                        </div>
+                <div class="login-img">
+                    <div class="container">
+                        <img src="../upload_system/<?=$db_system_banner?>" alt="">
                     </div>
                 </div>
             </div>
         </div>
+</div>
 
 
 
