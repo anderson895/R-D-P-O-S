d25f0b50cbc7386daa8ad55d240e8b4c1064d1f3
JARP <andersonandy046@gmail.com>
2024-10-30 12:43:52 +0800

    Update app.js

--- new-customer-website/javascript/app.js
+++ new-customer-website/javascript/app.js
@@ -322,30 +322,32 @@ $(document).on("click", ".btnViewProduct", function (e) {
 
   // Sent Messages
 
-  $("#fileInput").change(function() {
-    var file = this.files[0];
-    if (file) {
-        $("#fileName").text("Selected File: " + file.name);
-        $("#fileDisplay").show();
+  $('#fileInput').change(function() {
+    const file = this.files[0]; // Get the selected file
 
-        // Preview the image
-        var reader = new FileReader();
+    if (file) {
+        const reader = new FileReader(); // Create a FileReader to read the file
+
         reader.onload = function(e) {
-            $("#imagePreview").attr("src", e.target.result);
-            $("#imagePreviewContainer").show();
+            $('#imagePreview').attr('src', e.target.result); // Set the image src to the file data
+            $('#imagePreviewContainer').show(); // Show the image container
         };
-        reader.readAsDataURL(file);
+
+        reader.readAsDataURL(file); // Read the file as a Data URL
+
+        $('#fileName').append(file.name); // Display the file name
+        $('#fileDisplay').show(); // Show the file display
+        $('#sender_Messages').prop('disabled', true); // Disable the message input
     }
 });
 
-// Optional: For the remove file button
-$("#removeFile").click(function() {
-    $("#fileInput").val(''); // Clear the file input
-    $("#fileName").text("Selected File: ");
-    $("#imagePreviewContainer").hide(); // Hide image preview
-    $("#fileDisplay").hide(); // Hide file display
+$('#removeFile').click(function() {
+    $('#fileInput').val(''); // Clear the file input
+    $('#fileName').text('Selected File: '); // Reset the file name display
+    $('#imagePreviewContainer').hide(); // Hide the image preview
+    $('#fileDisplay').hide(); // Hide the file display
+    $('#sender_Messages').prop('disabled', false); // Enable the message input
 });
-
 
 
 $("#btnSentMessage").click(function (e) {
