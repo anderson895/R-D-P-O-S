3ae2b4ad4097707f9e76be2a4f6155eb0c532ab5
JARP <andersonandy046@gmail.com>
2024-10-30 15:05:19 +0800

    Update getViewMessage.js

--- administrator/admin_view/chat/javascript/getViewMessage.js
+++ administrator/admin_view/chat/javascript/getViewMessage.js
@@ -78,7 +78,7 @@ $(document).ready(function() {
                             messageHtml += (message.mess_sender == session_id) ? '' : '<i class="text-white"><b>' + message.acc_fname + ' ' + message.acc_lname + '</b> (' + message.acc_type + ')</i>';
                          
                             if (message.mess_content) {
-                                messageHtml += '<p>' + message.mess_content + '</p>';
+                                messageHtml += '<p class="text-dark">' + message.mess_content + '</p>';
                             }
 
 
