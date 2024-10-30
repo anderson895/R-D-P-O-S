d834109742c56c1708d067a2ae58997a1cbbfc7c
JARP <andersonandy046@gmail.com>
2024-10-30 12:36:16 +0800

    Update SentMessage.php

--- new-customer-website/backend/end-points/SentMessage.php
+++ new-customer-website/backend/end-points/SentMessage.php
@@ -6,10 +6,16 @@ $db = new global_class();
 $sender_id = $_POST['mess_sender_id'];
 $sender_Messages = $_POST['sender_Messages'];
 
+if(isset($_FILES)){
+    echo "<pre>";
+    print_r($_FILES);
+    echo "</pre>";
+}else{
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+}
 
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
 
 
     // if (isset($_POST['requestType'])) {
