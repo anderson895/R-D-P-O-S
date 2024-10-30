35fb75467376a4d7a31a52f2465c7fc75d1b5468
JARP <andersonandy046@gmail.com>
2024-10-30 12:22:44 +0800

    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">     <img id="imagePreview" class="img-fluid" style="max-height: 150px;" /> </div>

--- new-customer-website/message.php
+++ new-customer-website/message.php
@@ -92,7 +92,10 @@ include('components/header.php');
     <div id="fileDisplay" class="mt-2" style="display: none;">
         <span id="fileName">Selected File: </span>
         <div id="imagePreviewContainer" class="border rounded p-2 mt-2" style="display: none;">
-            <img id="imagePreview" class="img-fluid" style="max-height: 150px;"/>
+          <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+              <img id="imagePreview" class="img-fluid" style="max-height: 150px;" />
+          </div>
+
         </div>
         <button id="removeFile" class="btn btn-danger mt-2">X</button>
     </div>
