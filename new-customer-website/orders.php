68631aabcebba376aa7efd45c92788850409f3d3
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-14 16:13:59 +0800

    Update orders.php

--- new-customer-website/orders.php
+++ new-customer-website/orders.php
@@ -82,7 +82,7 @@ if (isset($_GET['page'])) {
                         <th class='text-center'>Order Date</th>
                        
                         <?= ($page == 'Delivered' || $page == 'Shipped') ? '<th class="text-center">Rider</th>' : '' ?>
-                        <?= ($page == 'Delivered' || $page == 'Shipped') ? '<th>Estimated Delivery</th>' : '' ?>
+                        <?= ($page == 'Delivered' || $page == 'Shipped') ? '<th class="text-center">Estimated Delivery</th>' : '' ?>
                         <?= ($page == 'Delivered') ? '<th>Delivery Date</th>' : '' ?>
                         <?= ($page == 'Cancelled') ? '<th>Cancellation Reason</th>' : '' ?>
                         <?= ($page == 'Rejected') ? '<th>Reject Reason</th>' : '' ?>
@@ -103,7 +103,7 @@ if (isset($_GET['page'])) {
                                 <td class='text-center'>₱<?= number_format($order['total'],2) ?></td>
                                 <td class='text-center'><?= date('F j, Y g:i A', strtotime($order['order_date'])) ?></td>
                                
-                                <?= ($page == 'Delivered' || $page == 'Shipped') ? '<td class="text-center">' . ucfirst($order['acc_fname']) . ' ' . $order['acc_lname'] . '</td>' : '' ?>
+                                <?= ($page == 'Delivered' || $page == 'Shipped') ? '<td>' . ucfirst($order['acc_fname']) . ' ' . $order['acc_lname'] . '</td>' : '' ?>
                                <?= ($page == 'Delivered'|| $page == 'Shipped') ? '<td>' . date('F j, Y', strtotime($order['estimated_delivery'] ?? '')) . '</td>' : '' ?>
                                <?= ($page == 'Delivered') ? '<td>' . date('F j, Y g:i A', strtotime($order['delivered_date'])) . '</td>' : '' ?>
                                <?= ($page == 'Cancelled') ? '<td>' . $order['cancel_reason'] . '</td>' : '' ?>
