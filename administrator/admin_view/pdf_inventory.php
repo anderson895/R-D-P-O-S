d4bbc48d59475f0c3326fcab343da013bf8bfe15
Fyke Lleva <floterina@gmail.com>
2024-11-15 01:49:02 +0800

    updated

--- administrator/admin_view/pdf_inventory.php
+++ administrator/admin_view/pdf_inventory.php
@@ -15,7 +15,7 @@
             <p class="m-0 p-0">rdeleon@gmail.com | 09876543211</p>
         </div>
         <div style="width: 20%">
-            <img style="width: 10%;" src="assets/img/print_logo.png" alt="">
+            <img style="width: 50%;" src="assets/img/print_logo.png" alt="">
         </div>
     </div>
 
