858c21c5bab22bfb9ba0c73029b1ab3b878843d4
Fyke Lleva <floterina@gmail.com>
2024-11-15 01:51:10 +0800

    updated

--- administrator/admin_view/pdf_inventory.php
+++ administrator/admin_view/pdf_inventory.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container pt-4 d-flex flex-row justify-content-between">
+    <div class="container">
         <div style="width: 80%">
             <h1 class="fw-bolder">Inventory Report</h1>
             <p class="m-0 p-0">R De Leon Poultry Supplies</p>
@@ -17,7 +17,6 @@
         <div style="width: 20%">
             <img style="width: 50%;" src="assets/img/print_logo.png" alt="">
         </div>
-        <hr>
     </div>
 
 
