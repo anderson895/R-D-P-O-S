09156b6ef9b2f8d7bbbebfd8cba78b67d4f8bd1b
Fyke Lleva <floterina@gmail.com>
2024-11-15 01:51:58 +0800

    updated

--- administrator/admin_view/pdf_inventory.php
+++ administrator/admin_view/pdf_inventory.php
@@ -7,15 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
-        <div style="width: 80%">
-            <h1 class="fw-bolder">Inventory Report</h1>
-            <p class="m-0 p-0">R De Leon Poultry Supplies</p>
-            <p class="m-0 p-0">Bagbaguin Sta. Maria Bulacan</p>
-            <p class="m-0 p-0">rdeleon@gmail.com | 09876543211</p>
-        </div>
-        <div style="width: 20%">
-            <img style="width: 50%;" src="assets/img/print_logo.png" alt="">
+    <div class="container ">
+        <div class="pt-4 d-flex flex-row justify-content-between">
+            <div style="width: 80%">
+                <h1 class="fw-bolder">Inventory Report</h1>
+                <p class="m-0 p-0">R De Leon Poultry Supplies</p>
+                <p class="m-0 p-0">Bagbaguin Sta. Maria Bulacan</p>
+                <p class="m-0 p-0">rdeleon@gmail.com | 09876543211</p>
+            </div>
+            <div style="width: 20%">
+                <img style="width: 50%;" src="assets/img/print_logo.png" alt="">
+            </div>
         </div>
     </div>
 
