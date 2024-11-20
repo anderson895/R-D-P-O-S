a96865ddc6ff5c62296ad59278e1bb0c674deea3
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-20 16:53:37 +0800

    Update get-change-order-status-buttons.php

--- administrator/new-orders-view/backend/endpoints/get-change-order-status-buttons.php
+++ administrator/new-orders-view/backend/endpoints/get-change-order-status-buttons.php
@@ -43,15 +43,19 @@ $current_time = date("H:i:s");
 // Compare current time with cutoff
 if ($current_time < $cutoff) {
     // If the current time is before the cutoff time, enable the element
-   $cutoffStatus = ?>
-     <button <?=$cutoffStatus?> class="btn btn-success btnUpgradeStatus" data-id="<?= $orderId ?>" data-currstats="<?= $orderStatus ?>"><i class="bi bi-check2"></i> Accept</button>
-     <button class="btn btn-danger btnRejectOrder" data-id="<?= $orderId ?>"><i class="bi bi-x-lg"></i> Reject</button>
-   <?php
+    $cutoffStatus = ''; // Empty status string for enabled state
+    ?>
+    <button <?=$cutoffStatus?> class="btn btn-success btnUpgradeStatus" data-id="<?= $orderId ?>" data-currstats="<?= $orderStatus ?>"><i class="bi bi-check2"></i> Accept</button>
+    <button class="btn btn-danger btnRejectOrder" data-id="<?= $orderId ?>"><i class="bi bi-x-lg"></i> Reject</button>
+    <?php
 } else {
     // If the current time is equal to or after the cutoff time, disable the element
-   $cutoffStatus = "The Rider is Cutoff";
+    $cutoffStatus = "disabled"; // Set 'disabled' status for the button
+    ?>
+    <button <?=$cutoffStatus?> class="btn btn-success btnUpgradeStatus" data-id="<?= $orderId ?>" data-currstats="<?= $orderStatus ?>"><i class="bi bi-check2"></i> Accept</button>
+    <button class="btn btn-danger btnRejectOrder" data-id="<?= $orderId ?>"><i class="bi bi-x-lg"></i> Reject</button>
+    <?php
 }?>
-
         <?=$cutoffStatus?>
           
         <?php
