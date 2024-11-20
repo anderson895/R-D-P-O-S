09a1049991d2de88bfa940d5efda9dfd904e942a
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-20 14:30:16 +0800

    Update editTogler.js

--- administrator/admin_view/stock_in/javascript/editTogler.js
+++ administrator/admin_view/stock_in/javascript/editTogler.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-  
   function updateStockTable() {
     var invoice_no = $("#invoice_no").val();
     $.ajax({
@@ -9,84 +8,76 @@ $(document).ready(function () {
       data: { invoice_no: invoice_no },
       dataType: "json",
       success: function (data) {
-        // console.log(data)
         $("#stockTableBody").empty();
-
-        // Append new data to the table
+  
+        // Get today's date
+        var today = new Date();
+  
         $.each(data, function (index, row) {
+          var expirationDate = new Date(row.s_expiration);
+          var diffTime = expirationDate - today; // Difference in milliseconds
+          var diffDays = diffTime / (1000 * 3600 * 24); // Convert ms to days
+  
           // Add your HTML generation logic here
           var html = "<tr>";
           html += '<td><input type="checkbox" id="EachCheckbox"></td>';
           html += "<td>" + row.prod_name + "</td>";
           html += "<td>" + row.s_stock_in_qty + "</td>";
           html += "<td>" + row.s_amount + "</td>";
-          html += "<td>";
-
-          html += row.unit_type;
-
-          html += "</td>";
+          html += "<td>" + row.unit_type + "</td>";
           html += "<td>" + row.prod_currprice + "</td>";
           html += "<td>" + row.s_supplierPrice + "</td>";
-        html += "<td>" + ((row.prod_currprice - row.s_supplierPrice).toFixed(2)) + "</td>";
-
-          html +=
-            "<td>" +
-            (row.s_expiration !== "0000-00-00"
-              ? row.s_expiration
-              : "No expiration") +
-            "</td>";
+          html += "<td>" + ((row.prod_currprice - row.s_supplierPrice).toFixed(2)) + "</td>";
+  
+          // Set expiration date cell
+          var expirationCell = row.s_expiration !== "0000-00-00" ? row.s_expiration : "No expiration";
+          html += "<td class='expiration-date'>" + expirationCell + "</td>";
           html += "<td>" + row.s_stockin_date + "</td>";
+  
           html += '<td class="text-end">';
           html +=
             '<button class="btn btn-sm border editTogler" data-bs-toggle="modal" data-bs-target="#edit" ' +
-            'data-db_prod_name="' +
-            row.prod_name +
-            '" ' +
-            'data-db_s_expiration="' +
-            row.s_expiration +
-            '" ' +
-            'data-db_s_supplierPrice="' +
-            row.s_supplierPrice +
-            '" ' +
-            'data-db_s_amount="' +
-            row.s_amount +
-            '" ' +
-            'data-db_prod_kg="' +
-            row.prod_kg +
-            '" ' +
-            'data-db_prod_ml="' +
-            row.prod_ml +
-            '" ' +
-            'data-db_prod_g="' +
-            row.prod_g +
-            '" ' +
-            'data-db_prod_image="' +
-            row.prod_image +
-            '" ' +
-            'data-db_prod_currprice="' +
-            row.prod_currprice +
-            '" ' +
-            'data-db_prod_expirationStatus="' +
-            row.prod_expirationStatus +
-            '" ' +
-            'data-db_prod_code="' +
-            row.prod_code +
-            '" ' +
-            'data-db_s_id="' +
-            row.s_id +
-            '" ' +
+            'data-db_prod_name="' + row.prod_name + '" ' +
+            'data-db_s_expiration="' + row.s_expiration + '" ' +
+            'data-db_s_supplierPrice="' + row.s_supplierPrice + '" ' +
+            'data-db_s_amount="' + row.s_amount + '" ' +
+            'data-db_prod_kg="' + row.prod_kg + '" ' +
+            'data-db_prod_ml="' + row.prod_ml + '" ' +
+            'data-db_prod_g="' + row.prod_g + '" ' +
+            'data-db_prod_image="' + row.prod_image + '" ' +
+            'data-db_prod_currprice="' + row.prod_currprice + '" ' +
+            'data-db_prod_expirationStatus="' + row.prod_expirationStatus + '" ' +
+            'data-db_prod_code="' + row.prod_code + '" ' +
+            'data-db_s_id="' + row.s_id + '" ' +
             ">Edit</button>";
           html +=
-            '<button class="btn btn-sm border btnRemove" data-db_s_id="' +
-            row.s_id +
-            '">Remove</button>';
+            '<button class="btn btn-sm border btnRemove" data-db_s_id="' + row.s_id + '">Remove</button>';
           html += "</td></tr>";
-
-          $("#stockTableBody").append(html);
+  
+          // Append row to the table
+          var $row = $(html);
+          
+          // Apply dynamic styling to the expiration date cell only
+          if (diffDays <= 60 && diffDays >= 0) {
+            // Change the color of the expiration date to red
+            $row.find('.expiration-date').css({
+              'background-color': 'red',
+              'color': 'white'
+            });
+          } else {
+            // Change the color of the expiration date to green
+            $row.find('.expiration-date').css({
+              'background-color': 'green',
+              'color': 'white'
+            });
+          }
+  
+          $("#stockTableBody").append($row);
         });
       },
     });
   }
+  
 
 
 
