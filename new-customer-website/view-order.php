f458204312a918dd6692bc923bc440414833ed40
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-20 16:11:43 +0800

    Update view-order.php

--- new-customer-website/view-order.php
+++ new-customer-website/view-order.php
@@ -64,8 +64,8 @@ if ($getAddress->num_rows > 0) {
 
 <div class="d-flex justify-content-between">
     <a href="orders.php?page=<?= $order['status'] ?>" class="btn" style="font-size: 20px;"><i
-            class="bi bi-arrow-return-left"></i> BackSSSSSSSSSS</a>
-        <?=$address['cutoff']?>
+            class="bi bi-arrow-return-left"></i> Back</a>
+       
      <?=
         ($order['status'] == 'Pending' || $order['status'] == 'Accepted') ? '<button type="button" class="btn btn-danger CancelModal" data-id="' . $orderId . '">
         <i class="bi bi-x-lg"></i> Cancel Order</button>' : '';
