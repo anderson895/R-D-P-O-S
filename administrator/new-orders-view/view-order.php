e4dd8616ad4a50638acca13c95434ab54b63ec1a
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-20 16:41:30 +0800

    Update view-order.php

--- administrator/new-orders-view/view-order.php
+++ administrator/new-orders-view/view-order.php
@@ -71,7 +71,8 @@ if($getAddress->num_rows > 0) {
 // Get the current time in HH:mm:ss format
 $current_time = date("H:i:s");
 
-echo "the current cut off is "+$cutoff;
+echo "the current cut off is $cutoff"
+
 // Compare current time with cutoff
 if ($current_time < $cutoff) {
     // If the current time is before the cutoff time, enable the element
