8bd95778395d2fc6ee2896c3086379fe83757ff0
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-20 17:02:02 +0800

    Update get-change-order-status-buttons.php

--- administrator/new-orders-view/backend/endpoints/get-change-order-status-buttons.php
+++ administrator/new-orders-view/backend/endpoints/get-change-order-status-buttons.php
@@ -51,13 +51,11 @@ if ($current_time < $cutoff) {
 } else {
     // If the current time is equal to or after the cutoff time, disable the element
    
-$cutoffStatus = '<div class="alert alert-danger border border-danger" role="alert">
-    <strong>Unable to Accept Orders: Cutoff Time Reached</strong>
-</div>'; // Message when cutoff is reached
+$cutoffStatus = 'Unable to Accept Orders: Cutoff Time Reached'; // Message when cutoff is reached
    
 }?>
 
-<?=$cutoffStatus?>
+        <?php echo $cutoffStatus?>
 
           
         <?php
