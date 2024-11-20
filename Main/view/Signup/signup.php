e9e1ecced7beb2de0a6e14456f327006f4bd6dd5
JARP <105678913+anderson895@users.noreply.github.com>
2024-11-20 12:44:08 +0800

    Update signup.php

--- Main/view/Signup/signup.php
+++ Main/view/Signup/signup.php
@@ -98,9 +98,7 @@
                     </div>
                 </div>
             </div>
-            <div class="login-img d-none d-md-block">
-                <img src="../upload_system/<?=$db_system_banner?>" alt="Login Image" class="img-fluid" style="max-height: 100vh; object-fit: cover;">
-            </div>
+           
         </div>
     </div>
 </div>
