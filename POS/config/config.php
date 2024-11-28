a1df1ae992d67f36c10700e423a6d423d8a10b95
JARP <andersonandy046@gmail.com>
2024-11-28 23:18:44 +0800

    debug connection limit

--- POS/config/config.php
+++ POS/config/config.php
@@ -1,5 +1,16 @@
 <?php
+// $connections = mysqli_connect ("localhost","root","","u547077750_rdpos");
+$connections = mysqli_connect ("localhost","u547077750_rdpos","Rdpos2024","u547077750_rdpos");
+?>
+
+<?php
 // Database credentials
+// $hostname = 'localhost';      // Change this to your database hostname
+//  $username = 'root';   // Change this to your database username
+// $password = '';   // Change this to your database password
+// $database = 'u547077750_rdpos';   // Change this to your database name
+
+
 $hostname = 'localhost';      // Change this to your database hostname
 $username = 'u547077750_rdpos';   // Change this to your database username
 $password = 'Rdpos2024';   // Change this to your database password
@@ -13,8 +24,5 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 
-// Your queries and logic here
 
-// Close the connection when done
-$conn->close();
 ?>
