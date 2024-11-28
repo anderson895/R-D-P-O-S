21ddc9987ab23d4f76cd68ddcb519cde2af6f0d3
JARP <andersonandy046@gmail.com>
2024-11-29 00:02:56 +0800

    sorting expiration

--- administrator/admin_view/stock_in/controller/get_stock_data.php
+++ administrator/admin_view/stock_in/controller/get_stock_data.php
@@ -2,12 +2,24 @@
 include("../.../../../../../connection.php");
 
 $invoice_no = $_POST["invoice_no"];
+$filter = isset($_POST['filter']) ? $_POST['filter'] : 'all'; // Default filter is 'all'
 
-// Fetch data from the database
+// Set the expiration condition based on the filter
+if ($filter == 'normal') {
+    $expiration_condition = "AND s.s_expiration > NOW()";
+} elseif ($filter == 'soon') {
+    $expiration_condition = "AND s.s_expiration BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 2 MONTH)";
+} elseif ($filter == 'expired') {
+    $expiration_condition = "AND s.s_expiration < NOW()";
+} else {
+    $expiration_condition = ""; // Show all items if 'all' filter is selected
+}
+
+// Fetch data from the database with the filter applied
 $view_query = mysqli_query($connections, "SELECT *
 FROM stocks s
 LEFT JOIN product p ON s.s_prod_id = p.prod_id
-WHERE s.s_invoice = '$invoice_no' AND s_status='1';");
+WHERE s.s_invoice = '$invoice_no' AND s.s_status = '1' $expiration_condition;");
 
 $data = array();
 
