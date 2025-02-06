eb3ba1c326489c18c0a65c536f4e1e058c25e7d1
JARP <105678913+anderson895@users.noreply.github.com>
2025-02-06 14:37:24 +0800

    awd

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // Perform the redirection using an absolute URL
-header("Location: Main/index.php");
-exit; // Make sure to exit after sending the header
+//header("Location: Main/index.php");
+//exit; // Make sure to exit after sending the header
 ?>
